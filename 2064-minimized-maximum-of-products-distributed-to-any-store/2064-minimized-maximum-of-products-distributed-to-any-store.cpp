eafbc4cf3ca727e2cdae45fb8adddf05b4d8eb94
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-01 20:25:40 +0530

    Time: 297 ms (53.88%), Space: 84.3 MB (82.08%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int partition(vector<int>& q, int mid)
+    {
+        int p = 0;
+        for(auto x : q)
+        {
+            while(x > 0)
+            {
+                x -= mid;
+                p++;
+            }
+        }
+        return p;
+    }
+    int minimizedMaximum(int n, vector<int>& q) {
+        int right = INT_MIN;
+        for(auto x : q)
+        {
+            // mn = min(mn, x);
+            right = max(right, x);
+        }
+        int left = 1;
+        int ans = 0;
+        while(left <= right)
+        {
+            int mid = (left + right)/2;
+            if(partition(q, mid) <= n)
+            {
+                right = mid - 1;
+                ans = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
