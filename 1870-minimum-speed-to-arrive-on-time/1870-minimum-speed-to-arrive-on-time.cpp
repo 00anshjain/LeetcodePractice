2e6d1fde7843e20b66d23fb64b306a61b7f9ed46
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-09 09:50:59 +0530

    Time: 369 ms (59.11%), Space: 101.6 MB (45.83%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,41 +1,82 @@
 class Solution {
-private:
-    int f(int n,int m){
-        if(n%m !=0 ) return (n/m)+1;
-        else return (n/m);
-    }
-private:
-    bool isPossible(int mid,vector<int>& dist, double hour){
-        double cnt_hr=0;
-        
-        for(int i=0;i<dist.size()-1;i++){
-            cnt_hr+=f(dist[i],mid);
-        }
-        
-        cnt_hr+=(double)dist.back()/(double)mid;
-        
-        return cnt_hr<=hour;
-    }
 public:
-    int minSpeedOnTime(vector<int>& dist, double hour) {
-        if((1.0*dist.size()-1) >= hour) return -1; 
-        
-        int lo=1;
-        int hi=1e7;
-        
-        int ans=hi;
-        
-        while(lo<=hi){
-            
-            int mid=lo+(hi-lo)/2;
- 
-            if(isPossible(mid,dist,hour)){
-                ans=mid;
-                hi=mid-1;
-            }
-            else lo=mid+1;
+    typedef long long ll;
+    int n;
+    double error = 1e-9;
+    // typedef long long ll;
+    bool possible(int mid, vector<int>& dist, double hour)
+    {
+        // cout<<endl<<mid<<" ";
+        for(int i = 0; i < n-1; i++)
+        {
+            //cout<<hour<<":"<<ceil(dist[i]/(double)mid)<<"   "<<endl;
+            hour -= ceil(dist[i]/(double)mid);
+            // cout<<hour<<endl;
+            if(hour < 0)
+                return false;
         }
+        double z = hour - ((double)dist[n-1]/(double)mid);
+        //cout<<hour<<":"<<((double)dist[n-1]/(double)mid)<<"   "<<endl;
+        //cout<<hour - ((double)dist[n-1]/(double)mid);
+        if(z >= 0 || (-1*z) < error)
+        {
+            
+            // cout<<"  a   "<<(hour >= ((double)dist[n-1]/(double)mid));
+            return true;
+            
+        }
+        else
+        {
+            // cout<<"   b  "<<(hour >= ((double)dist[n-1]/(double)mid));
+            return false;
+        }
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        // ll sum = accumulate(dist.begin(), dist.end(), 0);
+         n = dist.size();
+        // int minSpeed = floor(sum/hour);
+        // if(ceil(hour) < n)
+        //     return -1;
+        // while(true)
+        // {
+        //     double t = 0;
+        //     for(int i = 0; i < n; i++)
+        //     {
+        //         ll x = dist[i];
         
+        //         if(i == n-1)
+        //             t = x/(double)minSpeed;
+        //         else
+        //             t += ceil(x/(double)minSpeed);
+        //         if(t > hour)
+        //             break;
+        //     }
+        //     if(t > hour)
+        //         minSpeed++;
+        //     else
+        //         return minSpeed;
+        // }
+        int left = 1;
+        int right = 1e7;
+        if( hour <= n-1)
+            return -1;
+        int ans = left;
+        
+        while(left <= right)
+        {
+            int mid = (left + right)/2;
+            if(possible(mid, dist, hour))
+            {
+                // cout<<"YES";
+                ans = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                // cout<<"No";
+                left = mid+1;
+            }
+        }
         return ans;
     }
 };
