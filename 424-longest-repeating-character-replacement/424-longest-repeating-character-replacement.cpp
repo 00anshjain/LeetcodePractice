8df3b57d9b83a511d720c4986d92baca3b80141f
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-12 17:10:16 +0530

    Time: 12 ms (70.03%), Space: 6.9 MB (84.15%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,60 +2,17 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        vector<int> j(26, 0);
-        vector<int> cnt(26, 0);
-        // int ja = 0, jb = 0;
-        // int cnta = 0, cntb = 0;
-        vector<int> sz(26, 0);
-        int mx = 0;//, asize = 0, bsize = 0;
+        int j = 0, longest = 0;
+        int res = 0;
+        unordered_map<char, int> mp;
         for(int i = 0; i < n; i++)
         {
-            for(int ch = 0; ch < 26; ch++)
-            {
-                sz[ch]++;
-                if(s[i] - 'A' != ch)
-                    cnt[ch]++;
-                while(cnt[ch] > k)
-                {
-                    if(s[j[ch]] - 'A' != ch)
-                        cnt[ch]--;
-                    j[ch]++;
-                    sz[ch]--;
-                }
-                mx = max(mx, sz[ch]);
-            }
+            mp[s[i]]++;
+            longest = max(longest, mp[s[i]]);
+            if(i - j + 1 - longest > k)
+                mp[s[j++]]--;
+            res = max(res, i - j + 1);
         }
-        //     if(s[i] !='B')
-        //     // {
-        //         cntb++;
-        //     // }
-        //     if(s[i] != 'A')
-        //         cnta++;
-        //     while(cnta > k)
-        //     {
-        //         if(s[ja] != 'A')
-        //         {
-        //             cnta--;
-        //         }
-        //         ja++;
-        //         asize--;
-        //         // ja++;
-        //     }
-        //     while(cntb > k)
-        //     {
-        //         if(s[jb] != 'B')
-        //         {
-        //             cntb--;
-        //         }
-        //         jb++;
-        //         bsize--;
-        //         // ja++;
-        //     }
-        //     mx = max(asize, mx);
-        //     mx = max(bsize, mx);
-        // }
-        return mx;
-	// cout<<mx<<endl;
-	
+        return res;
     }
 };
