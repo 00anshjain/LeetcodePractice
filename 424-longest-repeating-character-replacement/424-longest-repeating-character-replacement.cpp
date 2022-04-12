5da4dcfb1f7bbd1aba447ec4d7a2e080d4db2114
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-12 16:44:14 +0530

    Time: 157 ms (5.51%), Space: 7 MB (84.15%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        vector<int> j(26, 0);
+        vector<int> cnt(26, 0);
+        // int ja = 0, jb = 0;
+        // int cnta = 0, cntb = 0;
+        vector<int> sz(26, 0);
+        int mx = 0;//, asize = 0, bsize = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int ch = 0; ch < 26; ch++)
+            {
+                sz[ch]++;
+                if(s[i] - 'A' != ch)
+                    cnt[ch]++;
+                while(cnt[ch] > k)
+                {
+                    if(s[j[ch]] - 'A' != ch)
+                        cnt[ch]--;
+                    j[ch]++;
+                    sz[ch]--;
+                }
+                mx = max(mx, sz[ch]);
+            }
+        }
+        //     if(s[i] !='B')
+        //     // {
+        //         cntb++;
+        //     // }
+        //     if(s[i] != 'A')
+        //         cnta++;
+        //     while(cnta > k)
+        //     {
+        //         if(s[ja] != 'A')
+        //         {
+        //             cnta--;
+        //         }
+        //         ja++;
+        //         asize--;
+        //         // ja++;
+        //     }
+        //     while(cntb > k)
+        //     {
+        //         if(s[jb] != 'B')
+        //         {
+        //             cntb--;
+        //         }
+        //         jb++;
+        //         bsize--;
+        //         // ja++;
+        //     }
+        //     mx = max(asize, mx);
+        //     mx = max(bsize, mx);
+        // }
+        return mx;
+	// cout<<mx<<endl;
+	
+    }
+};
