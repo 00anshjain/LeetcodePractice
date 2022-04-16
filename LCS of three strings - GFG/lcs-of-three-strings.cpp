fd7c6309f01d04117de5f41c42642ee9ada1b639
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-16 15:56:59 +0530

    Added solution - LeetHub

--- LCS of three strings - GFG/lcs-of-three-strings.cpp
+++ LCS of three strings - GFG/lcs-of-three-strings.cpp
@@ -16,26 +16,24 @@ int main()
 // Contributed By: Pranay Bansal
 // } Driver Code Ends
 
-
+int recur(vector<vector<vector<int>>>& dp, int i, int j, int k, string &A, string &B, string &C)
+{
+    if(i == 0 || j == 0 || k == 0)
+        return 0;
+    if(dp[i][j][k] != -1)
+        return dp[i][j][k];
+    if(A[i-1] == B[j-1] && B[j-1] == C[k-1])
+    {
+        return dp[i][j][k] = 1 + recur(dp, i-1, j-1, k-1, A, B, C);
+    }
+    return dp[i][j][k] = max(max(recur(dp, i-1, j, k, A, B, C), recur(dp, i, j-1, k, A, B, C)), recur(dp, i, j, k-1, A, B, C));
+}
 int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
 {
-    int dp[n1+1][n2+1][n3+1];
-    memset(dp, 0, sizeof(dp));
-    for(int i = 1; i <= n1; i++)
-    {
-        for(int j = 1;  j <= n2; j++)
-        {
-            for(int k = 1; k <= n3; k++)
-            {
-                if(A[i-1] == B[j-1] && B[j-1] == C[k-1])
-                    dp[i][j][k] = 1 + dp[i-1][j-1][k-1];
-                else
-                {
-                    dp[i][j][k] = max(max(dp[i-1][j][k], dp[i][j-1][k]), dp[i][j][k-1]);
-                }
-            }
-        }
-    }
-    return dp[n1][n2][n3];
+    vector<vector<vector<int>>> dp(n1+1, vector<vector<int>>(n2+1, vector<int>(n3+1, -1)));
+    return recur(dp, n1, n2, n3, A, B, C);
+    
+    
+    // vector<int, vector<vector<int, vector<int>>> > v;
     // your code here
 }
