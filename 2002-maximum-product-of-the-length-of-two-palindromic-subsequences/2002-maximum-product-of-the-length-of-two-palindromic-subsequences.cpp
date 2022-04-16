37a62cb877a03245613733c41cc09ef16497f72e
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-16 22:40:09 +0530

    Time: 156 ms (91.34%), Space: 47.6 MB (12.52%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int ans, n;
-    bool isPalin(string s)
+    int ans = 0, n;
+    bool isPalin(string &s)
     {
         int i = 0, j = s.size() - 1;
         while(i < j)
         {
-            if(s[i++] != s[j--])
+            if(s[i] != s[j])
                 return false;
+            i++;
+            j--;
         }
         return true;
     }
-    int fun(string &s, int i, int j, vector<vector<int>> &dp)
+    int recur(string &s, int i, int j, vector<vector<int>> &dp)
     {
         if(i > j)
             return 0;
-        if(i == j)
-            return 1;
         if(dp[i][j] != -1)
             return dp[i][j];
+        if(i == j)
+            return dp[i][j] = 1;
         if(s[i] == s[j])
-            return dp[i][j] = 2 + fun(s, i+1, j-1, dp);
-        return max(fun(s, i, j-1, dp), fun(s, i+1, j, dp));
+            return dp[i][j] = 2 + recur(s, i+1, j-1, dp);
+        return dp[i][j] = max(recur(s, i+1, j, dp), recur(s, i, j-1, dp));
     }
     void func(string &s, int i, string curr, int mask)
     {
@@ -29,22 +31,22 @@ public:
         {
             if(!isPalin(curr))
                 return;
-            string check = "";
-            int k = curr.size();
+            string p = "";
             for(int j = 0; j < n; j++)
             {
-                if((mask & (1<<j))  == 0)
+                if(!(mask & (1<<j)))
                 {
-                    check += s[j];
+                    p += s[j];
                 }
             }
-            int sz = check.size();
-            vector<vector<int>> dp(sz, vector<int>(sz, -1));
-            ans = max(ans, k * fun(check, 0, sz - 1, dp));
+            int k = curr.size();
+            int sz = p.size();
+            vector<vector<int>> dp(sz + 1, vector<int>(sz+1, -1));
+            ans = max(ans, k * recur(p, 0, sz - 1, dp));
             return;
         }
         func(s, i+1, curr, mask);
-        func(s, i+1, curr + s[i], mask | (1<<i));
+        func(s, i+1, curr+ s[i], mask | (1<<i));
     }
     int maxProduct(string s) {
         ans = 0;
