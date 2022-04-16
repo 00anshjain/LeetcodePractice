178e7c53d4443ae1a1446883d6a7cf5af2f04a7f
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-16 17:46:06 +0530

    Time: 184 ms (61.34%), Space: 56.4 MB (54.52%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int, int> mp;
-        int mx = 0;
+    int longestSubsequence(vector<int>& arr, int d) {
+//         unordered_map<int, int> mp;
+//         int mx = 0;
+//         int n = arr.size();
         int n = arr.size();
-        
+        unordered_map<int,int> mp;
+        int ans = -1;
         for(int i = 0; i < n; i++)
         {
-            if(difference == 0)
-            {
-                mp[arr[i]]++;
-                mx = max(mx, mp[arr[i]]);
-                continue;
-            }
-            if(mp.find(arr[i]) == mp.end())
+            if(mp[arr[i] - d] == 0)
                 mp[arr[i]] = 1;
-            // int k = mp[arr[i]];
-            if(mp.find(arr[i] - difference) != mp.end())
-            {
-                mp[arr[i]] = max(mp[arr[i]], mp[arr[i] - difference] + 1);
-            }
-            // if(mp.find(arr[i] + difference) != mp.end())
-            // {
-            //     mp[arr[i]] = max(mp[arr[i]], mp[arr[i] + 1] + 1);
-            // }
-            mx = max(mp[arr[i]], mx);
+            else
+                mp[arr[i]] = 1 + mp[arr[i] - d];
+            ans = max(ans,mp[arr[i]]);
+//             if(difference == 0)
+//             {
+//                 mp[arr[i]]++;
+//                 mx = max(mx, mp[arr[i]]);
+//                 continue;
+//             }
+//             if(mp.find(arr[i]) == mp.end())
+//                 mp[arr[i]] = 1;
+//             // int k = mp[arr[i]];
+//             if(mp.find(arr[i] - difference) != mp.end())
+//             {
+//                 mp[arr[i]] = max(mp[arr[i]], mp[arr[i] - difference] + 1);
+//             }
+//             // if(mp.find(arr[i] + difference) != mp.end())
+//             // {
+//             //     mp[arr[i]] = max(mp[arr[i]], mp[arr[i] + 1] + 1);
+//             // }
+//             mx = max(mp[arr[i]], mx);
             
         }
-        return mx;
+        return ans;
+        // return mx;
         
     }
 };
