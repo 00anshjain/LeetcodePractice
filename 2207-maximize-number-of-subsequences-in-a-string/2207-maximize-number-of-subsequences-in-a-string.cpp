7aff6bb7e93e6ef0b202e65a54b4fdbc8671cba4
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-04-17 11:05:25 +0530

    Time: 45 ms (98.43%), Space: 16.6 MB (97.09%) - LeetHub

--- 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
@@ -2,40 +2,16 @@ class Solution {
 public:
     typedef long long ll;
     long long maximumSubsequenceCount(string text, string pattern) {
-        int n = text.size();
-        ll ans = 0;
-        ll mx = 0;
-        vector<ll> arr(n);
-        ll x = 0;
-        if(pattern[0] == pattern[1])
-            x = -1;
-        ll second = 0, first = 0;
-        for(ll i = 0; i < n; i++)
-        {
-            if(text[i] == pattern[0])
-            {
-                x++;
-                first++;
+        ll res = 0, cnt1 = 0, cnt2 = 0;
+        for (auto & c: text) {   
+            if (c == pattern[1]){
+                res += cnt1;
+                cnt2++;
             }
-            arr[i] = x;
-            if(text[i] == pattern[1])
-            {
-                second++;
-                // mx = max(mx, arr[i]);
-                ans += arr[i];
-            }
+            if (c == pattern[0])
+                cnt1++;
         }
-        // for(int i = 0; i < n; i++)
-        //     cout<<arr[i]<<" ";
-        // cout<<endl;
-        // if(ans == 0)
-        return ans + max(first, second);
-        
-        
-        
-        
-        
-        
+        return res + max(cnt1, cnt2);
         
         
     }
