b97701c9e8cd7d4ae9b40995ad01630486469450
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-05-27 13:02:02 +0530

    Time: 109 ms (97.07%), Space: 66.2 MB (55.75%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& arr) {
-        unordered_map<int, int> mp;
-        int i = 0, j = 0;
-        int unq = 0;
-        int cnt = 0, ans = 0;
-        int n = arr.size();
-        for(auto x : arr)
+    int totalFruit(vector<int>& fruits) {
+        int f1 = -1, f2 = -1;
+        //f1 for second last fruit, f2 for last fruit encountered
+        int same_fruit = 0, temp_ans = 0, ans = 0;
+        for(auto f : fruits)
         {
-            if(mp[x] == 0)
+            if(f == f1)
             {
-                while(unq > 1)
-                {
-                    mp[arr[i]]--;
-                    // i++;
-                    if(mp[arr[i]] == 0)
-                        unq -= 1;
-                    i++;
-                    cnt -= 1 ;
-                }
-                unq += 1;
+                same_fruit = 1; //count for last fruit encountered is 1
+                temp_ans++;
+                f1 = f2;
+                f2 = f; // since last fruit encountered is f
             }
-            mp[x]++;
-            cnt+= 1;
-            ans = max(ans, cnt);
+            else if(f == f2)
+            {
+                same_fruit += 1;
+                temp_ans++;
+            }
+            else
+            {
+                temp_ans = same_fruit + 1; // now answer willl be last fruit same count + 1 for new fruit
+                same_fruit = 1; // new fruit encountered
+                f1 = f2;
+                f2 = f; // new fruit encountered is last fruit
+            }
+            ans = max(ans, temp_ans);
         }
         return ans;
+        
     }
 };
