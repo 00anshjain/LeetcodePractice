902fb3c27298e1b2db43cdbb0a004ccc8d6a54ee
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-04 13:50:05 +0530

    Time: 289 ms (60.89%), Space: 39.2 MB (96.28%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
+    unordered_set<string> hasFormed;
+    bool canForm(string w, unordered_set<string> &dict)
+    {
+        if(hasFormed.count(w))
+            return true;
+        int n = w.size();
+        for(int i = 1; i < n; i++)
+        {
+            string left = w.substr(0, i);
+            string right = w.substr(i);
+            if(dict.find(left) != dict.end())
+            {
+                if(dict.find(right) != dict.end() || canForm(right, dict)) 
+                {
+                    hasFormed.insert(w);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> dict(words.begin(), words.end());
         vector<string> res;
         for(string w : words)
         {
-            int n = w.size();
-            if(n == 0)
-                continue;
-            dict.erase(w);
-            vector<bool> dp(n+1, false);
-            dp[0] = true;
-            for(int j = 1; j <= n; j++)
+            if(canForm(w, dict))
             {
-                for(int i = j-1; i>= 0; i--)
-                {
-                    if(dp[i] && (dict.find(w.substr(i, j-i)) != dict.end()) )
-                    {
-                        dp[j] = true;
-                        break;
-                    }
-                }
-            }
-            if(dp[n])
+                // hasFormed.insert(w);
                 res.push_back(w);
-            dict.insert(w);
+            }
         }
+        // for(string w : words)
+        // {
+        //     int n = w.size();
+        //     if(n == 0)
+        //         continue;
+        //     dict.erase(w);
+        //     vector<bool> dp(n+1, false);
+        //     dp[0] = true;
+        //     for(int j = 1; j <= n; j++)
+        //     {
+        //         for(int i = j-1; i>= 0; i--)
+        //         {
+        //             if(dp[i] && (dict.find(w.substr(i, j-i)) != dict.end()) )
+        //             {
+        //                 dp[j] = true;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     if(dp[n])
+        //         res.push_back(w);
+        //     dict.insert(w);
+        // }
         return res;
     }
 };
