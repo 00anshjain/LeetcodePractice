5477a3cb045f50ce90695403b041f056b71e64ec
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-06 17:26:29 +0530

    Time: 21 ms (60.57%), Space: 14.3 MB (59.42%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,27 +9,17 @@
  */
 class Solution {
 public:
-    bool inorder(TreeNode* root, TreeNode* n1, TreeNode* n2)
-    {
-        if(!root)
-            return false;
-        if(root == n1 || root == n2)
-            return true;
-        if(inorder(root->right, n1, n2) || inorder(root->left, n1, n2))
-            return true;
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* n1, TreeNode* n2) {
-        // if(!root)
-        //     return NULL;
+        if(!root)
+            return NULL;
         if(root == n1 || root == n2)
             return root;
-        bool k1 = inorder(root->left, n1, n2);
-        bool k2 = inorder(root->right, n1, n2);
+        TreeNode* k1 = lowestCommonAncestor(root->left, n1, n2);
+        TreeNode* k2 = lowestCommonAncestor(root->right, n1, n2);
         if(k1 && k2)
             return root;
         if(k1)
-            return lowestCommonAncestor(root->left, n1, n2);
-        return lowestCommonAncestor(root->right, n1, n2);
+            return k1;
+        return k2;
     }
 };
