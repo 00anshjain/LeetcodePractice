619546dc77045fc9dd8dfc76f9cf656580aa4bc9
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-06 13:01:10 +0530

    Added solution - LeetHub

--- Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.cpp
+++ Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.cpp
@@ -12,37 +12,23 @@ class Solution {
     int checkMirrorTree(int n, int e, int A[], int B[]) {
         // code here
         
-        map<int, deque<int>> Amap, Bmap;
+        unordered_map<int, stack<int>> mp;
         for(int i = 0;i < 2*e; i += 2)
         {
-            Amap[A[i]].push_back(A[i+1]);
-            Bmap[B[i]].push_back(B[i+1]);
+            mp[A[i]].push(A[i+1]);
+            // Bmap[B[i]].push_back(B[i+1]);
         }
-        // for(auto a : mp.begin())
-        if(Amap.size() != Bmap.size())
-            return 0;
-        auto itr1 = Amap.begin();
-        // auto itr2 = Bmap.begin();
-        // while(itr1 != Amap.end() && itr2 != Bmap.end())
-        while(itr1 != Amap.end())
+        for(int i = 0;i < 2*e; i += 2)
         {
-            auto q1 = itr1->second;
-            auto q2 = Bmap[itr1->first];
-            int n1 = q1.size(), n2 = q2.size();
-            if(n1 != n2)
-                return false;
-            while(n1-- > 0)
-            {
-                if(q1.front() != q2.back())
-                    return false;
-                q1.pop_front();
-                q2.pop_back();
-                
-            }
-            itr1++;
-            // itr2++;
+            if(mp[B[i]].empty())
+                return 0;
+            if(mp[B[i]].top() != B[i+1])
+                return 0;
+            mp[B[i]].pop();
+            // Bmap[B[i]].push_back(B[i+1]);
         }
         return true;
+        // for(auto a : mp.begin())
             
     }
 };
