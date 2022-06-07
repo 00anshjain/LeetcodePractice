cc155622233d29271411c29cdd02254af34a5b82
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-07 11:53:27 +0530

    Time: 240 ms (24.06%), Space: 54.7 MB (35.10%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    void DFS(int u, vector<int> adj[], vector<bool> &visited)
+    {
+        visited[u] = true;
+        for(auto x : adj[u])
+        {
+            if(!visited[x])
+                DFS(x, adj, visited);
+        }
+    }
     int makeConnected(int n, vector<vector<int>>& con) {
         if(con.size() < n-1)
             return -1;
@@ -8,39 +17,18 @@ public:
         {
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
-            
         }
-        vector<int> dist(n, INT_MAX);
-        int cnt = 0;
         vector<bool> visited(n, false);
-        for(int i = 0; i < n; i++)
+        int cnt = 0;
+        for(int i = 0; i <n; i++)
         {
             if(!visited[i])
             {
                 cnt++;
-                priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-                pq.push({0, i});
-                dist[i] = 0;
-                // visited[i] = true;
-                while(!pq.empty())
-                {
-                    int u = pq.top().second;
-                    int d = pq.top().first;
-                    pq.pop();
-                    if(visited[u])
-                        continue;
-                    visited[u] = true;
-                    for(auto v : adj[u])
-                    {
-                        if(dist[v] == INT_MAX || dist[v] >= d + 1)
-                        {
-                            dist[v] = dist[u] + 1;
-                            pq.push({dist[v],v});
-                        }
-                    }
-                }
+                DFS(i, adj, visited);
             }
         }
         return cnt-1;
+        
     }
 };
