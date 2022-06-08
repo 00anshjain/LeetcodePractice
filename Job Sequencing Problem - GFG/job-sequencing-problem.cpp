ade23adbeaea6116faa1260d85c977bcd50e4abd
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-08 15:45:23 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -27,49 +27,28 @@ class Solution
 {
     public:
     //Function to find the maximum profit and the number of jobs done.
-    static bool comp(Job j1, Job j2)
+    static bool comp(Job &j1, Job &j2)
     {
         return j1.profit > j2.profit;
-    }
-    int find(vector<int> &parent, int i)
-    {
-        if(parent[i] == i)
-            return i;
-        return parent[i] = find(parent, parent[i]);
-    }
-    void union_gr(vector<int> &parent, int x, int y)
-    {
-        parent[x] = y;
     }
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         sort(arr, arr+n, comp);
-        int mxTime = 0;
-        for(int i = 0; i < n; i++)
-            mxTime = max(mxTime, arr[i].dead);
-        vector<int> parent(mxTime+1);
-        for(int i = 1; i <= mxTime; i++)
-        {
-            parent[i] = i;
-        }
-        vector<int> res(mxTime+1, -1);
-        for(int i = 0; i < n; i++)
-        {
-            int availSlot = find(parent, arr[i].dead);
-            if(availSlot > 0)
-            {
-                parent[availSlot] = availSlot-1;
-                res[availSlot] = i;
-            }
-        }
-        int cnt = 0;
+        // int dead[101] = {-1};
+        vector<int> dead(101, -1);
         int pro = 0;
-        for(int i = 1; i<= mxTime; i++)
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
         {
-            if(res[i] != -1)
+            for(int j = arr[i].dead; j>0; j--)
             {
-                cnt++;
-                pro += arr[res[i]].profit;
+                if(dead[j] == -1)
+                {
+                    dead[j] = arr[i].profit;
+                    pro += dead[j];
+                    cnt++;
+                    break;
+                }
             }
         }
         return {cnt, pro};
