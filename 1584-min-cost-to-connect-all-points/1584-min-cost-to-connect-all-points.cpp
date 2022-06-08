b57af22fba8713b1e7d80b7e16811f1254166e90
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-08 13:44:40 +0530

    Time: 90 ms (94.26%), Space: 10.2 MB (85.61%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
+        int curr = 0;
         int n = points.size();
-        vector<pair<int, int>> adj[n];
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j)
-                    continue;
-                adj[i].push_back({j, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])});
-            }
-            
-        }
         int ans = 0;
-        int cnt = 0;
         vector<bool> visited(n, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> distance(n, INT_MAX);
-        pq.push({0, 0});
-        distance[0] = 0;
-        while(cnt != n)
+        vector<int> dist(n, INT_MAX);
+        dist[0] = 0;
+        // for(int k = 0; k < n-1; k++)
+        while(1)
         {
-            auto t = pq.top();
-            pq.pop();
-            int u = t.second;
-            if(visited[u])
-                continue;
-            int d = t.first;
-            ans += d;
-            visited[u] = true;
-            cnt++;
-            for(auto x : adj[u])
+            visited[curr] = true;
+            for(int i = 0; i < n; i++)
             {
-                if(!visited[x.first] && x.second < distance[x.first])
-                {
-                    distance[x.first] = x.second;
-                    pq.push({x.second, x.first});
-                }
+                if(visited[i])
+                    continue;
+                dist[i] = min(dist[i], abs(points[curr][0]-points[i][0])+abs(points[curr][1]-points[i][1]));
             }
+            curr=min_element(dist.begin(),dist.end())-dist.begin();
+            if(dist[curr] == INT_MAX)
+                return ans;
+            ans += dist[curr];
+            dist[curr] = INT_MAX;
         }
         return ans;
     }
