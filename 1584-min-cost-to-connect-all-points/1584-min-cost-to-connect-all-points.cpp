23e78f727f2179cf13552640ec0e94c44831d563
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-08 13:04:16 +0530

    Time: 285 ms (64.65%), Space: 101.9 MB (31.10%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<pair<int, int>> adj[n];
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == j)
+                    continue;
+                adj[i].push_back({j, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])});
+            }
+            
+        }
+        int ans = 0;
+        vector<bool> visited(n, false);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> distance(n, INT_MAX);
+        pq.push({0, 0});
+        distance[0] = 0;
+        while(!pq.empty())
+        {
+            auto t = pq.top();
+            pq.pop();
+            int u = t.second;
+            if(visited[u])
+                continue;
+            int d = t.first;
+            ans += d;
+            visited[u] = true;
+            for(auto x : adj[u])
+            {
+                if(!visited[x.first] && x.second < distance[x.first])
+                {
+                    distance[x.first] = x.second;
+                    pq.push({x.second, x.first});
+                }
+            }
+        }
+        return ans;
+    }
+};
