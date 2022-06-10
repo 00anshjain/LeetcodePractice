2df0c6c93ab5833f7d6c1e5eb70fe9a0b48f75e8
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-11 02:24:15 +0530

    Time: 6 ms (96.88%), Space: 9 MB (78.44%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int getCount(vector<int> &nums, int target, int n, vector<vector<int>> &dp)
-    {
-        // if(target == 0)
-        //     return 1;
-        if(n == 0)
-        {
-            if(target == 0)
-                return 1;
-            return 0;
-        }if(dp[n][target + 2000] != -1)
-            return dp[n][target + 2000];
-        // if(nums[n-1] <= target)
-        // {
-            return dp[n][target + 2000] = getCount(nums, target + nums[n-1], n-1, dp) + getCount(nums, target - nums[n-1], n-1, dp);
-        // }
-        // return dp[n][target + 1000] = getCount(nums, target, n-1, dp);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        // S1 - S2 = target -> eqn1
+        // S1 + S2 = sum    -> eqn2
+        //  eqn1 + eqn1 => 2S1 = target + sum
+        // S1 = (target + sum) / 2
+        // So our question reduces to count number of ways to get Sum S1.
+        
+        int t = (target + sum +1 )/2;
+        if(t < 0 || t > sum || (target + sum)%2 != 0)//|| ((sum + t) %2 != 0))
+            return 0;
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(4002, -1));
-        return getCount(nums, target, n, dp);
+        int dp[n+1][t+1];
+        for(int i = 1; i < t+1; i++)
+            dp[0][i] = 0;
+        for(int i = 0; i < n+1; i++)
+            dp[i][0] = 1;
+        int cntZero = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(nums[i-1] == 0)
+                cntZero++;
+            for(int j = 1; j <= t; j++)
+            {
+                // dp[i][j] = dp[i-1][j];
+                if(j >= nums[i-1] && nums[i-1] != 0)
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return pow(2, cntZero)*dp[n][t];
+        
+        
     }
 };
