8dfa9c55420c271258157e66a4643dca74e65173
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-11 20:23:39 +0530

    Time: 181 ms (15.35%), Space: 10.4 MB (38.15%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int cutStick(int l, int r, vector<vector<int>> &dp, vector<int> &cuts, int left, int right)
+    int cutStick(int l, int r, vector<vector<int>> &dp, vector<int> &cuts)
     {
-        if(l == r)
-            return right - left;
-        if(l > r)
+        if(l + 1 >= r)
             return 0;
         if(dp[l][r]  != -1)
             return dp[l][r];
-        int cost = right - left;
+        // int cost = right - left;
         int finalAns = INT_MAX;
-        for(int i = l; i <= r; i++)
+        for(int i = l+1; i < r; i++)
         {
-            int ans = cutStick(l, i-1, dp, cuts, left, cuts[i]) + cutStick(i+1, r, dp, cuts, cuts[i], right);
+            int ans = cutStick(l, i, dp, cuts) + cutStick(i, r, dp, cuts) + cuts[r] - cuts[l];
             finalAns = min(finalAns, ans);
         }
-        return dp[l][r] = cost + finalAns;
+        // if(finalAns == INT_MAX)
+        //     return dp[l][r] = cuts[r] - cuts[l];
+        return dp[l][r] = finalAns;
     }
     int minCost(int n, vector<int>& cuts) {
-        int m = cuts.size();
+        cuts.push_back(n);
+        cuts.push_back(0);
         sort(cuts.begin(), cuts.end());
+        int m = cuts.size();
         vector<vector<int>> dp(m+1, vector<int>(m+1, -1));
-        return cutStick(0, m-1, dp, cuts, 0, n);
+        return cutStick(0, m-1, dp, cuts);
     }
 };
