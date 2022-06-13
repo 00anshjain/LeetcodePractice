5070ff04575d41506c7241ea9ae435972d725aca
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-14 00:16:46 +0530

    Time: 142 ms (86.96%), Space: 23.7 MB (91.30%) - LeetHub

--- 1771-maximize-palindrome-length-from-subsequences/1771-maximize-palindrome-length-from-subsequences.cpp
+++ 1771-maximize-palindrome-length-from-subsequences/1771-maximize-palindrome-length-from-subsequences.cpp
@@ -1,56 +1,43 @@
 class Solution {
 public:
-    int ans = 0;
-    int recur(string &s, int i, int j, vector<vector<int>> &dp, int n1, int n2)
-    {
-        // if(i == 0 || j == 0)
-        //     return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(i > j)
-            return dp[i][j] = 0;
-        else if(i == j)
-            return dp[i][j] = 1; 
-        else if(s[i] == s[j])
-        {
-            dp[i][j] =  2 + recur(s, i+1, j-1, dp, n1, n2);
-            if(i < n1 && j >= n1)
-                ans = max(ans, dp[i][j]);
-            return dp[i][j];
-        }
-        // else
-            return dp[i][j] = max(recur(s, i, j-1, dp, n1, n2), recur(s, i+1, j, dp, n1, n2));
-        // cout<<dp[i][j]<<" ";
-        // return dp[i][j];
-    }
-    int longestPalindrome(string s1, string s2) {
-        string s = s1 + s2;
-        ans = 0;
-        int n1 = s1.size();
-        int n2 = s2.size();
+    int longestPalindrome(string word1, string word2) {
+        string s = word1 + word2;
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        recur(s, 0, n-1, dp, n1, n2);
+        int dp[n][n];
+        for(int i = 0; i < n-1; i++)
+        {
+            dp[i][i] = 1;
+            if(s[i] == s[i+1])
+                dp[i][i+1] = 2;
+            else
+                dp[i][i+1] = 1;
+        }
+        dp[n-1][n-1] = 1;
+        for(int i = n-3; i >= 0; i--)
+        {
+            for(int j = i+2; j< n; j++)
+            {
+                if(s[i] == s[j])
+                {
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                }
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        int n1 = word1.size();
+        int n2 = word2.size();
+        int ans = 0;
+        for(int i = 0; i < n1; i++)
+        {
+            for(int j = 0; j < n2; j++)
+            {
+                if(word1[i] == word2[j])
+                {
+                    ans = max(ans, dp[i][n1 + j]);
+                }
+            }
+        }
         return ans;
-        // string k = s;
-        // reverse(k.begin(), k.end());
-        // int dp[n+1][n+1];
-        // int ans = 0;
-        // memset(dp, 0, sizeof(dp));
-        // for(int i = 1; i <= n; i++)
-        // {
-        //     for(int j = 1; j <= n; j++)
-        //     {
-        //         if(s[i-1] == k[j-1])
-        //             dp[i][j] = 1 + dp[i-1][j-1];
-        //         else
-        //             dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        //     }
-        // }
-        // if(dp[n][n1] == 0)
-        //     return 0;
-        // else if(dp[n][n] - dp[n][n1] == 0)
-        //     return 0;
-        // return dp[n][n];
     }
 };
