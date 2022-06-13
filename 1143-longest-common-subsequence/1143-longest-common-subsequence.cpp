afce45722bbf4e93df634d073366c7e456957aa3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-13 16:57:53 +0530

    Time: 18 ms (78.19%), Space: 10.6 MB (87.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    int recur(string &s, string &w, int n, int m, vector<vector<int>> &dp)
-    {
-        if(n == 0 || m == 0)
-            return 0;
-        if(dp[n][m] != -1)
-            return dp[n][m];
-        if(s[n-1] == w[m-1])
-            return dp[n][m] = 1 + recur(s, w, n-1, m-1, dp);
-        return dp[n][m] = max(recur(s, w, n-1, m, dp), recur(s, w, n, m-1, dp));
+//     int recur(string &s, string &w, int n, int m, vector<vector<int>> &dp)
+//     {
+//         if(n == 0 || m == 0)
+//             return 0;
+//         if(dp[n][m] != -1)
+//             return dp[n][m];
+//         if(s[n-1] == w[m-1])
+//             return dp[n][m] = 1 + recur(s, w, n-1, m-1, dp);
+//         return dp[n][m] = max(recur(s, w, n-1, m, dp), recur(s, w, n, m-1, dp));
 
-    }
+//     }
     int longestCommonSubsequence(string s, string w) {
         int n = s.size(), m = w.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return recur(s, w, n, m, dp);
+        int dp[n+1][m+1];
+        for(int i = 0; i <= m; i++)
+            dp[0][i] = 0;
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(s[i-1] == w[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+        
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+//         return recur(s, w, n, m, dp);
     }
 };
