918f7734e384d00b42fac2d75d1482c0098d4cf3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-14 10:40:46 +0530

    Time: 126 ms (93.47%), Space: 44 MB (14.95%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -3,48 +3,86 @@ public:
     int n, ans;
     bool isPalin(string &s)
     {
-        int k = s.size();
-        if(k <= 1)
-            return true;
-        int i = 0;
-        k--;
+        int i = 0, k = s.size()-1;
         while(i < k)
         {
-            if(s[i] != s[k])
+            if(s[i++] != s[k--])
                 return false;
-            k--;
-            i++;
         }
         return true;
         
     }
-    void recur(int i, string& s1, string& s2, string &s)
+    int LPS(string &s)
+    {
+        int k = s.size();
+        if(k <= 1)
+            return k;
+        vector<vector<int>> dp(k, vector<int>(k));
+        int ans = 0;
+        for(int i = 0; i < k-1; i++)
+        {
+            dp[i][i] = 1;
+            ans = 1;
+            if(s[i] == s[i+1])
+            {
+                dp[i][i+1] = 2;
+                ans = 2;
+            }
+            else
+                dp[i][i+1] = 1;
+        }
+        dp[k-1][k-1] = 1;
+        ans = max(ans, dp[k-1][k-1]);
+        for(int i = k-3; i >= 0; i--)
+        {
+            for(int j = i+2; j< k; j++)
+            {
+                if(s[i] == s[j])
+                {
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                    ans = max(ans, dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return ans;
+    }
+    void recur(int i, string& s1, string &s, int mask)
     {
         if(i == n)
         {
-            if(isPalin(s1) && isPalin(s2))
+            if(isPalin(s1))
             {
+                string g = "";
+                for(int j = 0; j < n; j++)
+                {
+                    if(!(mask & (1<<j)))
+                    {
+                        g += s[j];
+                    }
+                }
+                int k = s1.size();
+                int l = LPS(g);
                 // cout<<s1<<" "<<s2<<endl;
                 
-                ans = max(ans, (int)(s1.size()*s2.size()));
+                ans = max(ans, k*l);
             }
             return;
         }
-        recur(i+1, s1, s2, s);
-        s1.push_back(s[i]);
-        recur(i+1, s1, s2, s);
+        recur(i+1, s1, s, mask);
+        s1 += s[i];
+        recur(i+1, s1, s, mask | (1<<i));
         s1.pop_back();
-        s2.push_back(s[i]);
-        recur(i+1, s1, s2, s);
-        s2.pop_back();
-        
-        
     }
     int maxProduct(string s) {
         ans = 0;
         n = s.size();
-        string s1 = "", s2 = "";
-        recur(0, s1, s2, s);
+        int mask = 0;
+        string s1 = "";
+        recur(0, s1, s, mask);
         return ans;
     }
 };
