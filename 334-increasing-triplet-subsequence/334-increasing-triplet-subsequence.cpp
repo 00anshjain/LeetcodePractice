ab54d81941142b5d60dbcd7dc2e3371a3e9ac5de
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 23:49:27 +0530

    Time: 151 ms (8.48%), Space: 61.6 MB (68.02%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -4,21 +4,20 @@ public:
         int n = nums.size();
         if(n < 3)
             return false;
-        int mn[n];
-        int mx[n];
-        mn[0] = INT_MAX;
-        mx[n-1] = INT_MIN;
-        for(int i = 1; i < n; i++)
+        int first_min = INT_MAX, second_min = INT_MAX;
+        for(auto x : nums)
         {
-            mn[i] = min(mn[i-1], nums[i-1]);
-            mx[n-i-1] = max(mx[n-i], nums[n-i]);
-        }
-        for(int i = 1; i < n-1; i++)
-        {
-            if(mn[i] < nums[i] && mx[i] > nums[i])
+            if(x <= first_min)
+            {
+                first_min = x;
+            }
+            else if(x <= second_min)
+            {
+                second_min = x;
+            }
+            else
                 return true;
         }
         return false;
-        
     }
 };
