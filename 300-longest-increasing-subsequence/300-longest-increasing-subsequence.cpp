f6bd9d4126c2e26ba7c96019b9638654c58979c7
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 21:23:20 +0530

    Time: 7 ms (96.54%), Space: 10.5 MB (41.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,12 +6,12 @@ public:
         dp[0] = INT_MIN;
         for(auto x : nums)
         {
-            int i = upper_bound(dp.begin(), dp.end(), x) - dp.begin();
-            if(x < dp[i] && x > dp[i-1])
-            {
+            int i = lower_bound(dp.begin(), dp.end(), x) - dp.begin();
+            // if(x < dp[i] && x > dp[i-1])
+            // {
                 dp[i] = x;
                 ans = max(ans, i);
-            }
+            // }
             // cout<<x<<" "<<dp[i]<<" "<<dp[i-1]<<" "<<i<<endl;
         }
         return ans;
