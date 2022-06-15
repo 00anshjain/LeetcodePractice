fada918b5536c44c16b8fab5d24b280954a34933
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 21:09:49 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -9,28 +9,42 @@ class Solution
 {
     public:
     //Function to find length of longest increasing subsequence.
-    int recur(int j, int a[], vector<int> &dp)
-    {
-        if(j < 0)
-            return 0;
-        if(dp[j] != -1)
-            return dp[j];
-        int ans = 1;
-        for(int i = j-1; i>= 0; i--)
-        {
-            if(a[i] < a[j])
-                ans = max(ans, 1 + recur(i, a, dp));
+    // int recur(int j, int a[], vector<int> &dp)
+    // {
+    //     if(j < 0)
+    //         return 0;
+    //     if(dp[j] != -1)
+    //         return dp[j];
+    //     int ans = 1;
+    //     for(int i = j-1; i>= 0; i--)
+    //     {
+    //         if(a[i] < a[j])
+    //             ans = max(ans, 1 + recur(i, a, dp));
                 
-        }
-        return dp[j] = ans;
-    }
+    //     }
+    //     return dp[j] = ans;
+    // }
     int longestSubsequence(int n, int a[])
     {
         // vector<vector<int>> dp(n, vector<int>(n, -1));
-        vector<int> dp(n, -1);
+        // vector<int> dp(n, -1);
+        // int ans = 0;
+        // for(int i = 0; i < n; i++)
+        //     ans =  max(ans, recur(i, a, dp));
+        // return ans;
+        vector<int> dp(n, 1);
         int ans = 0;
         for(int i = 0; i < n; i++)
-            ans =  max(ans, recur(i, a, dp));
+        {
+            for(int j = i-1; j >= 0; j--)
+            {
+                if(a[j] < a[i])
+                {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
         return ans;
        // your code here
     }
