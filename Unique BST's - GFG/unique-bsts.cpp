56cbb01b43c2a64ff623fdcb0477152bb969934a
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 14:44:37 +0530

    Added solution - LeetHub

---
+++ Unique BST's - GFG/unique-bsts.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the total number of possible unique BST. 
+    int mod = 1e9+7;
+    int numTrees(int n) 
+    {
+        long long dp[n+1];
+        memset(dp, 0l, sizeof(dp));
+        dp[0] = 1l;
+        dp[1] = 1l;
+        for(int i = 2; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+                dp[i] += (dp[j] * dp[i-j-1])%mod;
+                dp[i] %= mod;
+        }
+        return dp[n];
+        // Your code here
+    }
+};
+
+// { Driver Code Starts.
+#define mod (int)(1e9+7)
+int main(){
+    
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //taking total number of elements
+        int n;
+        cin>>n;
+        Solution ob;
+        //calling function numTrees()
+        cout<<ob.numTrees(n)<<"\n";
+    }
+}	  // } Driver Code Ends
