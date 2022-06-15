7774086766ee44bab456abfc647f4cef1f98db86
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 21:22:41 +0530

    Time: 15 ms (76.68%), Space: 10.7 MB (20.52%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
-        // int n = arr.size();
-        // int dp[n+1];
-        // // vector<int> dp(n+1, INT_MAX);
-        // // memset(dp, INT_MAX, sizeof(dp));
-        // for(int i = 1; i <= n; i++)
-        //     dp[i] = INT_MAX;
-        // dp[0] = INT_MIN;
-        // for(auto x : arr)
-        // {
-        //     int i = upper_bound(dp, dp+n+1, x) - dp;
-        //     // int i = upper_bound(dp.begin(), dp.end(), x) - dp.begin();
-        //     if(dp[i] > x && dp[i-1] < x)
-        //         dp[i] = x;
-        // }
-        // int i;
-        // for(i = n; i >= 0; i--)
-        // {
-        //     if(dp[i] != INT_MAX)
-        //         break;
-        // }
-        // return i;
-        int n = a.size();
-        vector<int> dp(n, 1);
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size() + 1, INT_MAX);
         int ans = 0;
-        for(int i = 0; i < n; i++)
+        dp[0] = INT_MIN;
+        for(auto x : nums)
         {
-            for(int j = i-1; j >= 0; j--)
+            int i = upper_bound(dp.begin(), dp.end(), x) - dp.begin();
+            if(x < dp[i] && x > dp[i-1])
             {
-                if(a[j] < a[i])
-                {
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                }
+                dp[i] = x;
+                ans = max(ans, i);
             }
-            ans = max(ans, dp[i]);
+            // cout<<x<<" "<<dp[i]<<" "<<dp[i-1]<<" "<<i<<endl;
         }
         return ans;
+        
     }
 };
