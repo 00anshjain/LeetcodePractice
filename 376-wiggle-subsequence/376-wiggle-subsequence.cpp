adfbba0c8d96c28c98607202d1ef6f3fa77502fb
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-15 16:12:49 +0530

    Time: 3 ms (66.85%), Space: 7.1 MB (41.95%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
+        if(n < 1)
+                return 0;
+
         // int dp[n][2];
-        vector<vector<int>> dp(n, vector<int>(2, 1));
+        // vector<vector<int>> dp(n, vector<int>(2, 1));
         // memset(dp, 1, sizeof(dp));
+        int ma = 1, mi = 1;
         for(int i = 1; i < n; i++)
         {
-            for(int j = i-1; j >= 0; j--)
-            {
-                if(nums[j] > nums[i])
+            // for(int j = i-1; j >= 0; j--)
+            // {
+                if(nums[i] > nums[i-1])
                 {
-                    dp[i][0] = max(dp[i][0], dp[j][1] + 1);
+                    ma = mi + 1;
+                    // dp[i][0] = max(dp[i][0], dp[j][1] + 1);
                 }
-                else if(nums[j] < nums[i])
+                else if(nums[i] < nums[i-1])
                 {
-                    dp[i][1] = max(dp[i][1], dp[j][0] + 1);
+                    // dp[i][1] = max(dp[i][1], dp[j][0] + 1);
+                    mi = ma + 1;
                 }
-            }
+            // }
         }
-        int ans = 0;
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < 2; j++)
-                ans = max(ans, dp[i][j]);
-        }
-        return ans;
+        return max(ma, mi);
     }
 };
