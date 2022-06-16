255e695cc7e38e324f1ea0a3cca209eebb147dbe
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-17 02:21:06 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -9,54 +9,22 @@ class Solution{
     public:
     typedef long long ll;
     int mod = 1e9+7;
-    // ll ans = 0;
-    // void countWay(int n, int k,int lastColor, int taken)
-    // {
-    //     // cout<<n<<endl;
-    //     if(n == 0)
-    //     {
-    //         // cout<<ans<<endl;
-    //         ans++;
-    //         return;
-    //     }
-    //     for(int i = 0; i < k; i++)
-    //     {
-    //         if(i == lastColor)
-    //         {
-    //             if(taken == 2)
-    //                 continue;
-    //             else
-    //                 countWay(n-1, k, lastColor, taken+1);
-    //         }
-    //         else
-    //         {
-    //             countWay(n-1, k, i, 1);
-    //         }
-    //     }
-        
-    // }
     long long countWays(int n, int k){
-        
         if(n == 1)
             return k;
-        if(n== 2)
+        if(n == 2)
             return k*k;
-            
-        vector<ll> same(n+1, -1), diff(n+1, -1);
-        same[1] = k;
-        diff[1] = k;
-        diff[2] = k*(k-1);
-        same[2] = k;
+        ll same[n+1], diff[n+1];
+        same[1] = (ll)k;
+        diff[1] = (ll)k;
+        same[2] = (ll)k;
+        diff[2] = (ll)k * (k - 1);
         for(int i = 3; i <= n; i++)
         {
-            // cout<<i<<endl;
-            diff[i] = (diff[i-1]*(k-1) + same[i-1]*(k-1))%mod;
             same[i] = diff[i-1];
-            // cout<<diff[i] + same[i]<<endl;
+            diff[i] = ((same[i-1] + diff[i-1])*(k-1))%mod;
         }
-        
-        return (diff[n] + same[n])%mod;
-        
+        return (same[n]+diff[n])%mod;
         // code here
     }
 };
