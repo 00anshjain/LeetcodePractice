eb708fad5eb6b3e2126fa054d0052b1e5e4da884
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-17 13:19:00 +0530

    Added solution - LeetHub

--- Max length chain - GFG/max-length-chain.cpp
+++ Max length chain - GFG/max-length-chain.cpp
@@ -40,26 +40,29 @@ struct val{
 /*You are required to complete this method*/
 static bool comp(struct val a, struct val b)
 {
-    // if(a.first == b.first)
-    //     return a.second < b.second;
-    return a.first < b.first;
+    return a.second < b.second;
 }
 int maxChainLen(struct val p[],int n)
 {
-    sort(p, p+n, comp);
     vector<int> dp(n, 1);
-    int mx=0;
-    for(int i = n-1; i>= 0; i--)
+    sort(p, p+n, comp);
+    // memset(dp, 1, sizeof(dp));
+    // dp[0] = 1;
+    int mx = 0;
+    for(int i = 0; i < n; i++)
     {
-        for(int j = i+1; j< n; j++)
+        for(int j = i-1; j >= 0; j--)
         {
-            if(p[i].second < p[j].first)
+            if(p[j].second < p[i].first)
             {
-                dp[i] = max(dp[i], 1+dp[j]);
+                dp[i] = max(dp[i], dp[j] + 1);
             }
+            
         }
-        mx = max(dp[i], mx);
+        mx = max(mx, dp[i]);
     }
+    // for(int i = 0; i < n; i++)
+    //     cout<<dp[i]<<" ";
     return mx;
 //Your code here
 }
