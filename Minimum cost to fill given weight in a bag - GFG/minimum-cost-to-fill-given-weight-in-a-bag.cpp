ea49db82ce0cabd3cce36bcfe24e87e2c7ccbd61
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-18 02:20:58 +0530

    Added solution - LeetHub

--- Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.cpp
+++ Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.cpp
@@ -11,29 +11,24 @@ class Solution{
 		
 
 	public:
-	int recur(int n, vector<vector<int>> &dp, int cost[], int W)
-	{
-	    if(W == 0)
-	        return 0;
-	    if(n == 0)
-	        return INT_MAX;
-	    if(dp[n][W] != -1)
-	            return dp[n][W];
-	    int k1 = recur(n-1, dp, cost, W);
-	    int k2 = INT_MAX;
-	    if(n <= W && cost[n-1] != -1)
-	    {
-	        k2 = recur(n, dp, cost, W - n);
-	        if(k2 != INT_MAX)
-	            k2 += cost[n-1];
-	    }
-	    return dp[n][W] = min(k1, k2);
-	}
 	int minimumCost(int cost[], int N, int W) 
 	{ 
-	    vector<vector<int>> dp(N+1, vector<int>(W+1, -1));
-	    int k = recur(N, dp, cost, W);
-	    return k == INT_MAX ? -1 : k;
+	    vector<vector<int>> dp(N+1, vector<int> (W+1, INT_MAX));
+	    dp[0][0] = 0;
+	    for(int i = 1; i <= N; i++)
+	    {
+	        dp[i][0] = 0;
+	        for(int j = 1; j <= W; j++)
+	        {
+	            if(j >= i && cost[i-1] != -1 && dp[i][j-i] != INT_MAX)
+	            {
+	                dp[i][j] = min(dp[i-1][j], dp[i][j - i] + cost[i-1]);
+	            }
+	            else
+	                dp[i][j] = dp[i-1][j];
+	        }
+	    }
+	    return dp[N][W] == INT_MAX ? -1 : dp[N][W];
         // Your code goes here
 	} 
 };
