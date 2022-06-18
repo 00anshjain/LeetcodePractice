3ccf8a1a23ac63d02f6dba7e4760f064fefbc065
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-19 01:22:52 +0530

    Added solution - LeetHub

--- Find all possible palindromic partitions of a String - GFG/find-all-possible-palindromic-partitions-of-a-string.cpp
+++ Find all possible palindromic partitions of a String - GFG/find-all-possible-palindromic-partitions-of-a-string.cpp
@@ -8,9 +8,9 @@ using namespace std;
 class Solution {
   public:
   int n;
-    bool isPalin(string &s)
+    bool isPalin(string &s, int i, int j)
     {
-        int i = 0, j = s.size()-1;
+        // int i = 0, j = s.size()-1;
         while(i < j)
         {
             if(s[i++] != s[j--])
@@ -18,31 +18,35 @@ class Solution {
         }
         return true;
     }
-    void recur(string &s, string curr, vector<string> ans, vector<vector<string>> &res, int i)
+    void recur(string &s, vector<string> &ans, vector<vector<string>> &res, int i)
     {
         if(i == n)
         {
-            if(curr == "")
-            {
                 res.push_back(ans);
-            }
-            return;
         }
-        curr += s[i];
-        recur(s, curr, ans, res, i+1);
-        if(isPalin(curr))
+        for(int j = i; j < n; j++)
         {
-            ans.push_back(curr);
-            curr = "";
-            recur(s, curr, ans, res, i+1);
+            if(isPalin(s, i, j))
+            {
+                ans.push_back(s.substr(i, j-i+1));
+                recur(s, ans, res, j+1);
+                ans.pop_back();
+            }
         }
+        // curr += s[i];
+        // recur(s, curr, ans, res, i+1);
+        // if(isPalin(curr))
+        // {
+        //     ans.push_back(curr);
+        //     curr = "";
+        //     recur(s, curr, ans, res, i+1);
+        // }
     }
     vector<vector<string>> allPalindromicPerms(string S) {
         n = S.size();
-        string curr = "";
         vector<vector<string>> res;
         vector<string> ans;
-        recur(S, curr, ans, res, 0);
+        recur(S, ans, res, 0);
         sort(res.begin(), res.end());
         return res;
         // code here
