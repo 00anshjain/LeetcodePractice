b93a701f5a49d33d5c56bdf05fb5a7f7b6b7f04b
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-18 18:37:58 +0530

    Time: 193 ms (90.63%), Space: 78.4 MB (85.94%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -4,31 +4,22 @@ public:
         int n = nums.size();
         if(n == 1)
             return nums[0];
-        // int dp[n];
-        // vector<int> dp(n, INT_MIN);
-        // memset(dp, 0, sizeof(dp));
-        // dp[0] = nums[0];
-        // priority_queue<pair<int, int>> pq;
         deque<int> q;
         q.push_back(0);
         int j = 0;
         for(int i = 1; i < n; i++)
         {
-            while(i - q.front() > k)
+            // int j = q.front
+            if(i - q.front() > k)
                 q.pop_front();
             int val = nums[q.front()] + nums[i]; 
             // bcse largest value in queue is at front only
             nums[i] = val;
             while(!q.empty() && val > nums[q.back()])
                 q.pop_back();
+            // jitni choti values h purani hata do, vo kaam ki nhi rahi, 
+            //because badi better value aa gayi h
             q.push_back(i);
-            // if(i == n-1)
-            //     return pq.top().first + nums[i];
-            // pq.push({pq.top().first + nums[i], i});
-            // // for(int j = i-1; j>= 0 && j >= i-k; j--)
-            // {
-            //     dp[i] = max(dp[i], dp[j] + nums[i]);
-            // }
         }
         return nums[q.back()];
     }
