a7aaf4c11ae7db8009be19b8d593591b88711866
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-18 18:35:58 +0530

    Time: 310 ms (42.51%), Space: 78.4 MB (85.94%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -8,23 +8,28 @@ public:
         // vector<int> dp(n, INT_MIN);
         // memset(dp, 0, sizeof(dp));
         // dp[0] = nums[0];
-        priority_queue<pair<int, int>> pq;
-        pq.push({nums[0], 0});
+        // priority_queue<pair<int, int>> pq;
+        deque<int> q;
+        q.push_back(0);
         int j = 0;
         for(int i = 1; i < n; i++)
         {
-            while(i - j > k)
-                j++;
-            while(!pq.empty() && pq.top().second < j)
-                pq.pop();
-            if(i == n-1)
-                return pq.top().first + nums[i];
-            pq.push({pq.top().first + nums[i], i});
-            // for(int j = i-1; j>= 0 && j >= i-k; j--)
+            while(i - q.front() > k)
+                q.pop_front();
+            int val = nums[q.front()] + nums[i]; 
+            // bcse largest value in queue is at front only
+            nums[i] = val;
+            while(!q.empty() && val > nums[q.back()])
+                q.pop_back();
+            q.push_back(i);
+            // if(i == n-1)
+            //     return pq.top().first + nums[i];
+            // pq.push({pq.top().first + nums[i], i});
+            // // for(int j = i-1; j>= 0 && j >= i-k; j--)
             // {
             //     dp[i] = max(dp[i], dp[j] + nums[i]);
             // }
         }
-        return -1;
+        return nums[q.back()];
     }
 };
