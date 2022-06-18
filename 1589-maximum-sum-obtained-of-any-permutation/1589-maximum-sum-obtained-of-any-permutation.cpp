be37a1f8037a9b047f98ba1a83a6d6ccdc8cbc8d
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-18 12:27:42 +0530

    Time: 796 ms (25.72%), Space: 139.9 MB (15.43%) - LeetHub

---
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    typedef long long ll;
+    int mod = 1e9+7;
+    int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& requests) {
+        // unordered_map<int, int> mp;
+        int n = nums.size();
+        vector<int> req(n+1, 0);
+        for(auto x : requests)
+        {
+            int start = x[0];
+            int end = x[1];
+            req[start]++;
+            req[end+1]--;
+            // int i = start;
+            // while(i <= end)
+            // {
+            //     mp[i]++;
+            //     i++;
+            // }
+        }
+        for(int i = 1; i < n; i++)
+            req[i] = req[i-1] + req[i];
+        priority_queue<pair<int, int>> pq;
+        for(int i = 0; i < n; i++)
+            pq.push({req[i], i});
+        sort(nums.begin(), nums.end(), greater<int>());
+        int i = 0;
+        ll sum = 0;
+        while(!pq.empty())
+        {
+           sum += ((ll)nums[i]*pq.top().first)%mod;
+            pq.pop();
+            i++;
+            sum%= mod;
+        }
+        return (int)sum;
+    }
+};
