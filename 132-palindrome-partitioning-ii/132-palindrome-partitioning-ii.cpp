b98fc5fff955a48214f1acb0edde2e4e9d68f046
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-20 00:01:26 +0530

    Time: 27 ms (93.47%), Space: 8 MB (68.97%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int i, int j)
-    {
-        while(i <= j)
-        {
-            if(s[i] != s[j])    return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+    // bool isPalindrome(string &s, int i, int j)
+    // {
+    //     while(i <= j)
+    //     {
+    //         if(s[i] != s[j])    return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
     int solve(string &s, int i, int j, vector<vector<int>>& dp)
     {
         if(i >= j)  return 0;
         if(dp[i][j] != -1)  return dp[i][j];
-        if(isPalindrome(s, i, j))
-            return dp[i][j] = 0;
+        // if(palin[i][j])
+        //     return dp[i][j] = 0;
         int mn = INT_MAX;
         for(int k = i; k < j; k++)
         {
-            if(isPalindrome(s, i, k))
-            {
-                if(dp[k+1][j] == -1)  dp[k+1][j] = solve(s, k+1, j, dp);
-                //int tempans = solve(str, i, k, dp) + solve(str, k+1, j, dp) + 1;
-                mn = min(dp[k+1][j] + 1, mn);
-            }
-            
+            if(dp[i][k] == -1)  dp[i][k] = solve(s, i, k, dp);
+            if(dp[k+1][j] == -1)  dp[k+1][j] = solve(s, k+1, j, dp);
+            //int tempans = solve(str, i, k, dp) + solve(str, k+1, j, dp) + 1;
+            mn = min(dp[i][k] + dp[k+1][j] + 1, mn);
         }
         return dp[i][j] = mn;
     }
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, (vector<int>(n, -1)));
-        return solve(s, 0, n-1, dp);
+        int cut[n];
+        for(int i = 0; i < n; i++)
+            cut[i] = i;
+        //to find out how many cut needed for string of size i 
+        vector<vector<bool>> dp(n, (vector<bool>(n, false)));
+        for(int j = 0; j < n; j++)  // string start se end tak h,  end = j
+        {
+            for(int i = j; i >= 0; i--)     // start changing;
+            {
+                if(s[i] == s[j] && ((j - i + 1 <= 2) || dp[i+1][j-1]))
+                {
+                    dp[i][j] = true;
+                    if(i != 0)
+                        cut[j] = min(cut[j], cut[i-1] + 1);
+                    else
+                        cut[j] = 0;
+                        // i = 0 mtlb starting se yaha tak aaye koi cut nhi lagana pada
+                }
+            }
+        }
+        return cut[n-1];
     }
 };
