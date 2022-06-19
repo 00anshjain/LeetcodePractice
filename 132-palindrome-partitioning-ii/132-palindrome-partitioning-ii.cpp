51bd44e0cd6c9d7ef2819de4cc7177b58fe779d3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-20 00:01:58 +0530

    Time: 40 ms (88.79%), Space: 8 MB (70.06%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    // bool isPalindrome(string &s, int i, int j)
-    // {
-    //     while(i <= j)
-    //     {
-    //         if(s[i] != s[j])    return false;
-    //         i++;
-    //         j--;
-    //     }
-    //     return true;
-    // }
-    int solve(string &s, int i, int j, vector<vector<int>>& dp)
-    {
-        if(i >= j)  return 0;
-        if(dp[i][j] != -1)  return dp[i][j];
-        // if(palin[i][j])
-        //     return dp[i][j] = 0;
-        int mn = INT_MAX;
-        for(int k = i; k < j; k++)
-        {
-            if(dp[i][k] == -1)  dp[i][k] = solve(s, i, k, dp);
-            if(dp[k+1][j] == -1)  dp[k+1][j] = solve(s, k+1, j, dp);
-            //int tempans = solve(str, i, k, dp) + solve(str, k+1, j, dp) + 1;
-            mn = min(dp[i][k] + dp[k+1][j] + 1, mn);
-        }
-        return dp[i][j] = mn;
-    }
     int minCut(string s) {
         int n = s.size();
         int cut[n];
