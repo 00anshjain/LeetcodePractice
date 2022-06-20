a3cf81e6eb7da2e92db338c9d19b9c050fc683cf
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-21 00:23:26 +0530

    Time: 3 ms (53.01%), Space: 7.6 MB (94.72%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,13 +4,16 @@ public:
         int n = nums.size();
         if(n == 1)
             return nums[0];
-        int dp[n];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // int dp[n];
+        int notTakenLast = nums[0];
+        int takenLast = max(nums[0], nums[1]);
         for(int i = 2; i < n; i++)
         {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            int nnotTakenLast = max(takenLast, notTakenLast);
+            takenLast = notTakenLast + nums[i];
+            notTakenLast = nnotTakenLast;
+            // dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
         }
-        return dp[n-1];
+        return max(notTakenLast, takenLast);
     }
 };
