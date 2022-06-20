6455b60ab38e13347138655b12914d5f0306ded8
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-20 17:00:09 +0530

    Added solution - LeetHub

--- Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
@@ -10,30 +10,15 @@ class Solution {
     typedef long long ll;
     ll ans;
     int mod = 1e9+7;
-    int M, N;
-    ll DFS(int i, int j, vector<vector<ll>> &dp)
-    {
-        
-        if(i == M-1 && j == N-1)
-            return 1;
-        if(i >= M || j>= N)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int ans = 0l;
-        ans += DFS(i+1, j, dp);
-        ans %= mod;
-        ans += DFS(i, j+1, dp);
-        ans %= mod;
-        return dp[i][j] = ans;
-        
-    }
     long long int numberOfPaths(int m, int n){
         ans = 0;
-        M = m;
-        N = n;
-        vector<vector<ll>> dp(m+1, vector<ll>(n+1, -1));
-        return DFS(0, 0, dp);
+        vector<vector<ll>> dp(m+1, vector<ll>(n+1, 1));
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1])%mod;
+        }
+        return dp[m-1][n-1];
         // code here
     }
 };
