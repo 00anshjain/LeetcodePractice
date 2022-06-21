b6bf2bf812fd712e2492b076268946e014e72a70
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-21 18:14:32 +0530

    Time: 403 ms (30.42%), Space: 48.2 MB (47.69%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,60 +1,29 @@
 class Solution {
 public:
-    int binarySearch(vector<int> &stones, int l, int r, int val)
-    {
-        // int ans = -1;
-        while(l <= r)
+    unordered_map<int, set<int>> mp;
+    bool canCross(vector<int>& stones) {
+        set<int> st;
+        for(auto s : stones)
+            mp[s] = st;
+        mp[0].insert(1);
+        int n = stones.size();
+        for(int i = 0; i < n; i++)
         {
-            int mid = (l + r)/2;
-            if(stones[mid] == val)
-                return mid;
-            else if(stones[mid] > val)
-                r = mid - 1;
-            else if(stones[mid] < val)
-                l = mid + 1;
-        }
-        return -1;
-    }
-    bool recur(vector<int> &stones, int i, int k, int n, vector<vector<int>>& dp)
-    {
-        // cout<<stones[i]<<" "<<k<<endl;
-        if(k <= 0)
-            return false;
-        if(i == n-1)
-            return true;
-        if(dp[i][k] != -1)
-            return dp[i][k];
-        // bool ans = false;
-        int j = binarySearch(stones, i+1, n-1, stones[i] + k);
-        if(j != -1)
-        {
-            if(recur(stones, j, k, n, dp))
-            {   
-                return true;
+            int curr = stones[i];
+            for(auto jump : mp[curr])
+            {
+                if(mp.find(jump + curr) != mp.end())
+                {
+                    int newPos = jump+curr;
+                    if(newPos == stones[n-1])
+                        return true;
+                    if(jump -1 > 0)
+                        mp[newPos].insert(jump - 1);
+                    mp[newPos].insert(jump + 1);
+                    mp[newPos].insert(jump);
+                }
             }
         }
-        j = binarySearch(stones, i+1, n-1, stones[i] + k+1);
-        if(j != -1)
-        {
-            if(recur(stones, j, k+1, n, dp))
-                return true;
-        }
-        j = binarySearch(stones, i+1, n-1, stones[i] + k-1);
-        if(j != -1)
-        {
-            if(recur(stones, j, k-1, n, dp))
-                return true;
-        }
-        return dp[i][k] = false;
-    }
-    bool canCross(vector<int>& stones) {
-        int n = stones.size();
-        if(n == 1)
-            return true;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        if(stones[1] != 1)
-            return false;
-        return recur(stones, 1, 1, n, dp);
-        
+        return false;
     }
 };
