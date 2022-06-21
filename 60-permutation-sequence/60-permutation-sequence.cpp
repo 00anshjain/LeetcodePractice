7915ee5e42db4691c60fe30f19b2d65fc734e6ff
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-22 03:19:16 +0530

    Time: 9 ms (24.88%), Space: 6 MB (49.18%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        k--;
         int fact[n+1];
+        vector<int> num;
         fact[0] = 1;
         for(int i = 1; i <= n; i++)
+        {
+            num.push_back(i);
             fact[i] = i*fact[i-1];
-        bool taken[n+1];
-        memset(taken, false, sizeof(taken));
+        }
+        // bool taken[n+1];
+        
+        // memset(taken, false, sizeof(taken));
         string ans = "";
         while(n > 0)
         {
-            int j = 1;
-            while(taken[j])
-                j++;
-            while(k > fact[n-1])
-            {
-                k -= fact[n-1];
-                j++;
-                while(taken[j])
-                    j++;
+            // int j = 1;
+            // while(taken[j])
+            //     j++;
+            int z = k/fact[n-1];
+            ans += to_string(num[z]);
+            num.erase(num.begin() + z);
+            k %= fact[n-1];
             
-            }
-            taken[j] = true;
-            ans += to_string(j);
+//             {
+//                 k -= fact[n-1];
+//                 j++;
+//                 while(taken[j])
+//                     j++;
+            
+//             }
+//             taken[j] = true;
+            // ans += to_string(j);
             n--;
         }
         return ans;
