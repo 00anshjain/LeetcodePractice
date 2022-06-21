bd525c1154d7ef1b96639d21f89d25820ca371ff
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-21 14:36:15 +0530

    Time: 525 ms (40.34%), Space: 115.2 MB (77.02%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -3,23 +3,17 @@ public:
     typedef long long ll;
     long long mostPoints(vector<vector<int>>& ques) {
         int n = ques.size();
-        ll ans = 0l; 
-        vector<vector<ll>> dp(n, vector<ll>(2, 0l)); //isko leke profit, isko liye/bina liye maxm profit
-        // vector<vector<ll>> dp[n][2];   
-        // memset(dp, 0l, sizeof(dp));
+        vector<ll> dp(n, 0l);
+        ll ans = 0;
         for(int i = n-1; i >= 0; i--)
         {
             int end = i + ques[i][1] + 1;   //to get profit after idle time of this work
             if(end < n)
-               dp[i][0] = ques[i][0] + max(dp[end][0], dp[end][1]);
+               dp[i] = max(ans, ques[i][0] + dp[end]);
             else
-                dp[i][0] = ques[i][0];
-            ans = max(ans, dp[i][0]);
-            dp[i][1] = ans;
+                dp[i] = max(ans, (ll)ques[i][0]);
+            ans = max(ans, dp[i]);
         }
         return ans;
-        // for(int i = 0; i < n; i++)
-        //     ans = max(ans, dp[i]);
-        // return max(ans, dp[n]);
     }
 };
