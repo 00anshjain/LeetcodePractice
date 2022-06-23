c7aac8a90e7de838a8e03b6aa904980d524b602c
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-23 13:34:54 +0530

    Time: 25 ms (34.27%), Space: 13.2 MB (24.49%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -19,9 +19,26 @@ public:
     }
     int maxProfit(int k, vector<int>& prices) {
         n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        for(int ind = n-1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int cap = 1; cap <= k; cap++)
+                {
+                    if(buy)
+                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap] , 
+                                          dp[ind+1][1][cap]);
+                    else
+                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1] , 
+                                          dp[ind+1][0][cap]);
+                }
+                
+            }
+        }
+        return dp[0][1][k];
         // dp for index, buy(yes or no), capcity(0 - c)  
-        return recur(prices, dp, 0, 1, k); //index 0, buy true, capacity = k 
+        // return recur(prices, dp, 0, 1, k); //index 0, buy true, capacity = k 
         
         
     }
