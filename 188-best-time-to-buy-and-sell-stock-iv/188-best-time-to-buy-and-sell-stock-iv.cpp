69db6a8c02c35c7158322b63a316005a1162b17d
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-23 13:53:21 +0530

    Time: 4 ms (94.27%), Space: 11.7 MB (61.27%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int n;
-    int recur(vector<int>& prices, vector<vector<vector<int>>>& dp, int ind, int buy, int cap)
-    {
-        if(ind == n || cap == 0)
-            return 0;
-        if(dp[ind][buy][cap] != -1)
-            return dp[ind][buy][cap];
-        if(buy)
-        {
-            return dp[ind][buy][cap] = max(-prices[ind] + recur(prices, dp, ind+1, 0, cap) , 
-                                          0 + recur(prices, dp, ind+1, 1, cap));
-        }
-        else{
-            return dp[ind][buy][cap] = max(prices[ind] + recur(prices, dp, ind+1, 1, cap-1) , 
-                                          0 + recur(prices, dp, ind+1, 0, cap));
-        }
-    }
+    // int recur(vector<int>& prices, vector<vector<vector<int>>>& dp, int ind, int buy, int cap)
+    // {
+    //     if(ind == n || cap == 0)
+    //         return 0;
+    //     if(dp[ind][buy][cap] != -1)
+    //         return dp[ind][buy][cap];
+    //     if(buy)
+    //     {
+    //         return dp[ind][buy][cap] = max(-prices[ind] + recur(prices, dp, ind+1, 0, cap) , 
+    //                                       0 + recur(prices, dp, ind+1, 1, cap));
+    //     }
+    //     else{
+    //         return dp[ind][buy][cap] = max(prices[ind] + recur(prices, dp, ind+1, 1, cap-1) , 
+    //                                       0 + recur(prices, dp, ind+1, 0, cap));
+    //     }
+    // }
     int maxProfit(int k, vector<int>& prices) {
         n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        int dp[n+1][2][k+1];
+        memset(dp, 0, sizeof(dp));
         for(int ind = n-1; ind >= 0; ind--)
         {
             for(int buy = 0; buy <= 1; buy++)
