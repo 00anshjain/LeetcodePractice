f6e04c9b3fe32e4cf50e9001825768c79d7a44d3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-23 20:35:02 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.cpp
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -10,77 +10,96 @@ using namespace std;
 class Solution{
   public:
     /*You are required to complete this method */
-    bool recur(int i, int j, int k, vector<vector<int>> &dp, string &A, string &B, string &C)
-    {
-        if(k == 0)
-            return true;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        bool ans1 = false, ans2 = false;
-        // if(i != 0 && j != 0)
-        // {
-        //     bool ans1 = false, ans2 = false;
-        //     if(A[i-1] == C[K-1])
-        //         ans1 = recur(i-1, j, k-1, dp, A, B, C);
-        //     if(ans1)
-        //         return dp[i][j] = 1;
-        //     if(B[j-1] == C[K-1])
-        //         ans2 = recur(i, j-1, k-1, dp, A, B, C);
-        //     if(ans2)
-        //         return dp[i][j] = 1;
-        //     return dp[i][j] = 0;
-        // }
-        if(i != 0)
-        {
-            if(A[i-1] == C[k-1])
-                ans1 = recur(i-1, j, k-1, dp, A, B, C);
-            if(ans1)
-                return dp[i][j] = 1;
-        }
-        if(j != 0)
-        {
-            if(B[j-1] == C[k-1])
-                ans2 = recur(i, j-1, k-1, dp, A, B, C);
-            if(ans2)
-                return dp[i][j] = 1;
-        }
-        return dp[i][j] = 0;
-    }
+    // bool recur(int i, int j, int k, vector<vector<int>> &dp, string &A, string &B, string &C)
+    // {
+    //     if(k == 0)
+    //         return true;
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     bool ans1 = false, ans2 = false;
+    //     if(i != 0)
+    //     {
+    //         if(A[i-1] == C[k-1])
+    //             ans1 = recur(i-1, j, k-1, dp, A, B, C);
+    //         if(ans1)
+    //             return dp[i][j] = 1;
+    //     }
+    //     if(j != 0)
+    //     {
+    //         if(B[j-1] == C[k-1])
+    //             ans2 = recur(i, j-1, k-1, dp, A, B, C);
+    //         if(ans2)
+    //             return dp[i][j] = 1;
+    //     }
+    //     return dp[i][j] = 0;
+    // }
     bool isInterleave(string A, string B, string C) 
     {
         int i = 0, j = 0, k = 0;
         int n1 = A.size(), n2 = B.size(), n3 = C.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
         if(n1 + n2 != n3)
             return false;
-        return recur(n1, n2, n3, dp, A, B, C);
-        while(i < n1 && j < n2)
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[0][0] = true;
+        for(int i = 1; i <= n2; i++)
         {
-            if(C[k] == A[i])
-                i++;
-            else if(C[k] == B[j])
-                j++;
+            if(C[i-1] == B[i-1])
+                dp[0][i] = true;
             else
-                return false;
-            k++;
+                break;
         }
-        while(i < n1)
+        for(int i = 1; i <= n1; i++)
         {
-            if(C[k] == A[i])
-                i++;
+            if(C[i-1] == A[i-1])
+                dp[i][0] = true;
             else
-                return false;
-            k++;
+                break;
         }
-        while(j < n2)
+        
+        for(int i = 1; i <= n1; i++)
         {
-            if(C[k] == B[j])
-                j++;
-            else
-                return false;
-            k++;
+            for(int j = 1; j <= n2; j++)
+            {
+                int k = i+j;
+                if(A[i-1] == C[k-1])
+                {
+                    dp[i][j] =  dp[i][j] || dp[i-1][j];
+                }
+                if(B[j-1] == C[k-1])
+                {
+                    dp[i][j] = dp[i][j] || dp[i][j-1];
+                }
+            }
         }
-        return true;
+        return dp[n1][n2];
+        // return recur(n1, n2, n3, dp, A, B, C);
+        // while(i < n1 && j < n2)
+        // {
+        //     if(C[k] == A[i])
+        //         i++;
+        //     else if(C[k] == B[j])
+        //         j++;
+        //     else
+        //         return false;
+        //     k++;
+        // }
+        // while(i < n1)
+        // {
+        //     if(C[k] == A[i])
+        //         i++;
+        //     else
+        //         return false;
+        //     k++;
+        // }
+        // while(j < n2)
+        // {
+        //     if(C[k] == B[j])
+        //         j++;
+        //     else
+        //         return false;
+        //     k++;
+        // }
+        // return true;
         //Your code here
     }
 
