21d4f42f67867e4f96d88a9441f6cad540b8c1eb
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-23 17:55:40 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (40.08%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
-    bool recur(vector<int> &nums, int sum1, int sum2, bool turn, int i, int j)
-    {
-        if(i > j)
-            return sum1 >= sum2;
-        if(turn)
-        {
-            if(recur(nums, sum1 + nums[i], sum2, !turn, i+1, j) ||
-                    recur(nums, sum1 + nums[j], sum2, !turn, i, j-1))
-               return true;
-            return false;
-        }
-        else
-        {
-            if((!recur(nums, sum1, sum2 + nums[i], !turn, i+1, j) ||
-                    !recur(nums, sum1, sum2 + nums[j], !turn, i, j-1)))
-                return false;
-            return true;
-        }
-    }
     bool PredictTheWinner(vector<int>& nums) {
-        int sum1 = 0, sum2 = 0;
         int n = nums.size();
-        bool turn = true;
-        return recur(nums, sum1, sum2, turn, 0, n-1);
-        // int sum1 = 0, sum2 = 0;
-        // int i = 0, j = nums.size() - 1;
-        // bool flag = true;
-        // while(i <= j)
-        // {
-        //     if(flag)
-        //     {
-        //         if(nums[i] >= nums[j])
-        //             sum1 += nums[i++];
-        //         else
-        //             sum1 += nums[j--];
-        //     }
-        //     else
-        //     {
-        //         if(nums[i] >= nums[j])
-        //             sum2 += nums[i++];
-        //         else
-        //             sum2 += nums[j--];
-        //     }
-        //     flag = !flag;
-        // }
-        // return sum1 >= sum2;
+        if(n%2 == 0)
+            return true;
+        int dp[n][n];
+        memset(dp, 0, sizeof(dp));
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j = i; j < n; j++)
+            {
+                if(i == j)
+                    dp[i][j] = nums[i];
+                else
+                {
+                    int a = nums[i] - dp[i+1][j];
+                    int b = nums[j] - dp[i][j-1];
+                    dp[i][j] = max(a, b);
+                }
+            }
+        }
+        return dp[0][n-1] >= 0;
+        
     }
 };
