8214e9e82cf0fccf2cdcabbeb1e779767709f456
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-23 14:08:19 +0530

    Time: 185 ms (81.43%), Space: 77.9 MB (65.45%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
-    int n;
-    int recur(vector<int>& prices, vector<vector<vector<int>>>& dp, int ind, int buy, int cap)
-    {
-        if(ind == n || cap == 0)
-            return 0;
-        if(dp[ind][buy][cap] != -1)
-            return dp[ind][buy][cap];
-        if(buy)
-        {
-            return dp[ind][buy][cap] = max(-prices[ind] + recur(prices, dp, ind+1, 0, cap) , 
-                                          0 + recur(prices, dp, ind+1, 1, cap));
-        }
-        else{
-            return dp[ind][buy][cap] = max(prices[ind] + recur(prices, dp, ind+1, 1, cap-1) , 
-                                          0 + recur(prices, dp, ind+1, 0, cap));
-        }
-    }
+    // int n;
+    // int recur(vector<int>& prices, vector<vector<vector<int>>>& dp, int ind, int buy, int cap)
+    // {
+    //     if(ind == n || cap == 0)
+    //         return 0;
+    //     if(dp[ind][buy][cap] != -1)
+    //         return dp[ind][buy][cap];
+    //     if(buy)
+    //     {
+    //         return dp[ind][buy][cap] = max(-prices[ind] + recur(prices, dp, ind+1, 0, cap) , 
+    //                                       0 + recur(prices, dp, ind+1, 1, cap));
+    //     }
+    //     else{
+    //         return dp[ind][buy][cap] = max(prices[ind] + recur(prices, dp, ind+1, 1, cap-1) , 
+    //                                       0 + recur(prices, dp, ind+1, 0, cap));
+    //     }
+    // }
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        // dp for index, buy(yes or no), capcity(0 - c)  //here capcity = 2
-        return recur(prices, dp, 0, 1, 2); //index 0, buy true, capacity = 2 
-        
-        
+        int k = 2;
+        int n = prices.size();
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        //index from 0 to n-1, but 0 or 1, capacity 0 to k;
+        int dp[n+1][2][k+1];
+        memset(dp, 0, sizeof(dp));
+        for(int ind = n-1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int cap = 1; cap <= k; cap++)
+                {
+                    if(buy == 1)
+                    {
+                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
+                                                        dp[ind+1][1][cap]);
+
+                        // return dp[ind][buy][cap] = max(-prices[ind] + recur(dp, prices, ind+1, 0, cap), 
+                        //                                         recur(dp, prices, ind+1, 1, cap));
+                    }
+                    else
+                    {
+                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1], 
+                                                            dp[ind+1][0][cap]);
+                        // return dp[ind][buy][cap] = max(prices[ind] + recur(dp, prices, ind+1, 1, cap-1), 
+                        //                                     recur(dp, prices, ind+1, 0, cap));
+                    }
+                }
+            }
+        }
+        // recur(dp, prices, 0, 1, k);
+        return dp[0][1][k];
     }
 };
