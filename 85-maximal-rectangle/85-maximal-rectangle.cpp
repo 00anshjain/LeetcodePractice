5666d7a8d59866947cf9c07ca63dacf258265455
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-25 02:30:48 +0530

    Time: 49 ms (86.57%), Space: 12 MB (98.35%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-    typedef int ll;
-    int getAreaHistogram(vector<int> &ht, int n)
-    {
-        vector<int> stl(n, 0), str(n, n-1);
-        stack<int> st1, st2;
-        st1.push(0);
-        st2.push(n-1);
-        for(int i = 1; i < n; i++)
-        {
-            while(!st1.empty() && ht[st1.top()] >= ht[i])
-                st1.pop();
-            if(!st1.empty())
-                stl[i] = st1.top() + 1;
-            st1.push(i);
-        }
-        for(int i = n-2; i >= 0; i--)
-        {
-            while(!st2.empty() && ht[st2.top()] >= ht[i])
-                st2.pop();
-            if(!st2.empty())
-                str[i] = st2.top()-1;
-            st2.push(i);
-        }
-        int ans = 0;
-        for(int i = 0; i < n; i++)
-            ans = max(ans, ht[i]*(str[i] - stl[i] + 1));
-        return ans;
-    }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> ht(m, 0);
+        int dp[n][m];
+        memset(dp, 0, sizeof(dp));
         int ans = 0;
         for(int i = 0; i < n; i++)
         {
+            // dp[i][0] = matrix[i][0] == '0' ? 0 : dp[i-1][0] + 1;    
             for(int j = 0; j < m; j++)
             {
                 if(matrix[i][j] == '0')
-                    ht[j] = 0;
+                    continue;
+                if(i > 0)
+                    dp[i][j] = 1 + dp[i-1][j];
                 else
-                    ht[j] += 1;
+                    dp[i][j] = 1;
+                int k = j;
+                int smallest = dp[i][k];
+                while(k >= 0 && dp[i][k] > 0)
+                {
+                    smallest = min(smallest, dp[i][k]);
+                    ans = max(ans, smallest * (j - k + 1));
+                    k--;
+                }
             }
-            ans = max(ans, getAreaHistogram(ht, m));
         }
         return ans;
-        
     }
 };
