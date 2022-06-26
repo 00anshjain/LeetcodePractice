4111c3bc1413d094fec5b3bb0ff76f62193994ba
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-26 14:35:31 +0530

    Time: 2558 ms (5.01%), Space: 398.3 MB (5.01%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
+//     Using 0-1 DFS
     int xDir[4] = {0, 0, 1, -1};
     int yDir[4] = {1, -1, 0, 0};
-    bool isSafe(int r, int c, int n, int m)
+    bool isSafe(int r, int c, int n, int m, vector<vector<bool>> &visited)
     {
-        if(r < 0 || c < 0 || r >= n || c >= m)
+        if(r < 0 || c < 0 || r >= n || c >= m || visited[r][c])
             return false;
         return true;
     }
     int minimumObstacles(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        // priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        deque<vector<int>> q;
         if(grid[0][0] == 1)
-            pq.push({1, 0, 0});
+            q.push_back({1, 0, 0});
         else
-            pq.push({0, 0, 0});
+            q.push_back({0, 0, 0});
         if(n == 1 && m == 1)
-            return (pq.top())[0];
+            return (q.front())[0];
         // visited[0][0] = true;
-        dist[0][0] = (pq.top())[0];
-        while(!pq.empty())
+        // dist[0][0] = (q.front())[0];
+        while(!q.empty())
         {
-            auto t = pq.top();
-            pq.pop();
+            auto t = q.front();
+            q.pop_front();
             int r = t[1];
             int c = t[2];
             int d = t[0];
-            if(d > dist[r][c])
+            if(visited[r][c])
                 continue;
-            // if(visited[r][c])
-            //     continue;
-            // visited[r][c] = true;
+            visited[r][c] = true;
+            if(r == n-1 && c == m-1)
+                return d;
             for(int i = 0; i < 4; i++)
             {
                 int R = r + xDir[i];
                 int C = c + yDir[i];
-                if(!isSafe(R, C, n, m))
+                if(!isSafe(R, C, n, m, visited))
                     continue;
-                int D = d; 
                 if(grid[R][C] == 1)
-                    D = d + 1;
-                if(dist[R][C] > D)
-                {
-                    dist[R][C] = D;
-                    pq.push({D, R, C});
-                }
-                // visited[R][C] = true;
+                    q.push_back({d+1, R, C});
+                else
+                    q.push_front({d, R, C});
             }
             
         }
-        return dist[n-1][m-1];
+        return -1;
         
         
     }
