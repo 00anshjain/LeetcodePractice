fb71a4e41bec02e1f215d72e03ca61b8b50ddf32
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-27 19:39:19 +0530

    Time: 3 ms (90.26%), Space: 6.9 MB (87.30%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
+        // stack<int> st;
         //stack will contain the last unvalid poition
-        st.push(-1);
+        // st.push(-1);
         int n = s.size();
+        int start_bracket = 0;
+        int end_bracket = 0;
         int ans = 0;
         for(int i = 0; i < n; i++)
         {
             if(s[i] == '(')
-                st.push(i);
+                start_bracket++;
             else
             {
-                if(!st.empty())
+                end_bracket++;
+                if(start_bracket == end_bracket)
                 {
-                    st.pop();
+                    ans = max(ans, 2*end_bracket);
                 }
-                if(!st.empty())
+                else if(end_bracket > start_bracket)
                 {
-                    ans = max(ans, i - st.top()); //since st.top() contains nearest invalid poition
-                }
-                else
-                {
-                    st.push(i); //this means this closing bracket is extra and hence invalid so we add it to stack
+                    start_bracket = 0;
+                    end_bracket = 0;
                 }
             }
         }
+        // Now consider -> "()((()()"
+        // For this we start from backwards and similaryly like abvove, if we have 
+        // start_bracket > end_bracket, then we break and reset start and end to zero;
+        start_bracket = 0;
+        end_bracket = 0;
+        // int ans2 = 0;
+        for(int i = n-1; i >= 0; i--)
+        {
+            if(s[i] == ')')
+                end_bracket++;
+            else
+            {
+                start_bracket++;
+                if(end_bracket == start_bracket)
+                {
+                    ans = max(ans, 2*start_bracket);
+                }
+                else if(start_bracket > end_bracket)
+                {
+                    start_bracket = 0;
+                    end_bracket = 0;
+                }
+            }
+        }
+        
         return ans;
     }
 };
