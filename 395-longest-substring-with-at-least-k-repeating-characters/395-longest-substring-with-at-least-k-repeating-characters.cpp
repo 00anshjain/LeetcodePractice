15016243bfb45b5cc5cf7fc624434649352cf5a3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-28 02:22:25 +0530

    Time: 47 ms (39.45%), Space: 6.8 MB (61.46%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
-    int maxUnq(string &s)
-    {
-        vector<bool> taken(26, false);
-        int mx = 0;
-        for(auto c : s)
-        {
-            if(!taken[c-'a'])
-            {
-                mx++;
-                taken[c-'a'] = true;
-            }
-        }
-        return mx;
-    }
     int longestSubstring(string s, int k) {
-        int maxUnique = maxUnq(s); 
-        int ans = 0, n = s.size();
-        for(int currUnq = 1; currUnq <= maxUnique; currUnq++)
+        unordered_set<char> st(s.begin(), s.end());
+        int mxUnq = st.size();
+        int ans = 0;
+        int n = s.size();
+        for(int currUnq = 0; currUnq <= mxUnq; currUnq++)
         {
-            int j = 0, i = 0, unq = 0, kGreater = 0;
+            int unq = 0;
             unordered_map<char, int> mp;
-            while(j < n)
+            int i = 0;
+            int matched = 0;
+            for(int j = 0; j < n; j++)
             {
                 mp[s[j]]++;
                 if(mp[s[j]] == 1)
-                {
                     unq++;
-                }
                 if(mp[s[j]] == k)
-                    kGreater++;
+                    matched++;
                 while(unq > currUnq)
                 {
-                    
                     if(mp[s[i]] == k)
-                        kGreater--;
+                        matched--;
                     mp[s[i]]--;
                     if(mp[s[i]] == 0)
                         unq--;
                     i++;
-                    
                 }
-                if(kGreater == unq)
-                {
+                if(matched == unq)
                     ans = max(ans, j-i+1);
-                }
-                j++;
             }
         }
         return ans;
