9071d3fdb901476aaac9276c5a48e39110d3c36d
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-06-30 03:55:08 +0530

    Time: 129 ms (32.19%), Space: 14.7 MB (59.66%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,34 +1,55 @@
 class Solution {
 public:
-    typedef vector<int> vi;
+    // typedef vector<int> vi;
+    struct element{
+        int num;
+        int i;
+        int j;
+        int n;
+        element(int a, int b, int c, int d)
+        {
+            num = a;
+            i = b;
+            j = c;
+            n = d;
+        }
+    };
+    struct compare{
+      bool operator()(element &a, element &b)
+      {
+          return a.num > b.num;
+      }
+    };
+    
     vector<int> smallestRange(vector<vector<int>>& arr) {
         int k = arr.size();
         // int n = arr[0].size();
-        priority_queue<vi, vector<vi>, greater<vi>> pq;
-        int mx = 0;
+        priority_queue<element, vector<element>, compare> pq;
+        int mx = INT_MIN;
         int len = INT_MAX;
         vector<int> pr;
         for(int j = 0; j < k; j++)
         {
-            pq.push({arr[j][0], j, 0});
+            pq.push(element(arr[j][0], j, 0, arr[j].size()));
             mx = max(mx, arr[j][0]);
         }
-        while(1)
+        while(!pq.empty())
         {
-            auto v = pq.top();
+            element v = pq.top();
             pq.pop();
-            int num = v[0];
-            int i = v[1];
-            int j = v[2];
+            int num = v.num;
+            int i = v.i;
+            int j = v.j;
+            int n = v.n;
             if(len > mx - num)
             {
                 len = mx - num;
                 pr = {num, mx};
                 // cout<<pr[0]<<" "<<pr[1]<<" "<<j<<" "<<n-1<<endl;
             }
-            if(j == arr[i].size()-1)
+            if(j == n-1)
                 break;
-            pq.push({arr[i][j+1], i, j+1});
+            pq.push(element(arr[i][j+1], i, j+1, n));
             if(arr[i][j+1] > mx)
                 mx = arr[i][j+1];
         }
