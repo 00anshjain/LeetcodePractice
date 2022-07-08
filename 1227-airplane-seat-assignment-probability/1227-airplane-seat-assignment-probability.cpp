538f40337af0897531bb84ee35cdb7519b807416
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-08 13:12:11 +0530

    Time: 22 ms (5.31%), Space: 8.9 MB (33.12%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double nthPersonGetsNthSeat(int n) {
         if(n == 1)
-            return 1;
-        return 0.5;
+            return 1.0;
+        return (1.0/n)*1 + (n-2.0)/n*nthPersonGetsNthSeat(n-1);
     }
 };
