83c00057ae32f9c0e9e9c1356f32924bfb415ade
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-09 01:20:50 +0530

    Time: 23 ms (93.30%), Space: 13.7 MB (63.26%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
-    int findParent(vector<int> &par, int i)
+    int n;
+    void DFS(int u, vector<vector<int>> &con, vector<bool>& visited)
     {
-        if(par[i] == i)
-            return i;
-        return par[i] = findParent(par, par[i]);        
-    }
-    void getUnion(int i, int j, vector<int> &par, vector<int> &sz)
-    {
-        int p1 = findParent(par, i);
-        int p2 = findParent(par, j);
-        if(p1 != p2)
+        visited[u] = true;
+        for(int i = 0; i < n; i++)
         {
-            if(sz[p1] > sz[p2])
-            {
-                par[p2] = p1;
-                sz[p1] += sz[p2];
-            }
-            else
-            {
-                par[p1] = p2;
-                sz[p2] += sz[p1];
-            }   
+            if(con[u-1][i] && !visited[i+1])
+                DFS(i+1, con, visited);
         }
     }
     int findCircleNum(vector<vector<int>>& con) {
-        int n = con.size();
-        vector<int> par(n+1), sz(n+1);
-        for(int i = 1; i <= n; i++)
-        {
-            par[i] = i;
-            sz[i] = i;
-        }
-        for(int i = 0; i< n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j)
-                    continue;
-                if(!con[i][j])
-                    continue;
-                getUnion(i+1, j+1, par, sz);
-            }
-        }
+        n = con.size();
         int ans = 0;
-        for(int i = 1; i <= n; i++)
+        vector<bool> visited(n+1, false);
+        for(int i = 1; i <=n; i++)
         {
-            if(par[i] == i)
+            if(!visited[i])
+            {
                 ans++;
+                DFS(i, con, visited);
+            }
         }
         return ans;
     }
