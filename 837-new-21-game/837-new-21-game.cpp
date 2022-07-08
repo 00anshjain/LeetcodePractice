ddda6c048592da9fada2b417797b77fc240ed7e0
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-08 17:07:04 +0530

    Time: 7 ms (55.02%), Space: 6 MB (94.81%) - LeetHub

--- 837-new-21-game/837-new-21-game.cpp
+++ 837-new-21-game/837-new-21-game.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    double new21Game(int n, int k, int maxPoints) {
-        if(n < k)
-            return 0;
-        int end = k + maxPoints;
-        if(n >= end)
+    double new21Game(int n, int k, int maxPts) {
+        
+        if(n >= k + maxPts)
             return 1;
         if(k == 0)
             return 1;
-        vector<double> dp(n+1);
-        double percnt = 1.0;
-        dp[0] = 1.0;
-        int i;
-        for(int i = 1; i <= maxPoints && i < k; i++)
+        double dp[n+1];
+        dp[0] = 0.0;
+        // dp[0] = 1.0;
+        double sum = 0.0;
+        double res = 0.0;
+        for(int i = 1; i <= n; i++)
         {
-            dp[i] = (1.0/maxPoints)*percnt;
-            percnt += dp[i];
+            // if(i < k)
+            dp[i] = i <= maxPts ? ((1.0/maxPts)*sum + (1.0/maxPts)) : ((1.0/maxPts)*sum);
+            
+            if(i < k)
+                sum += dp[i];
+            if(i > maxPts)
+                sum -= dp[i-maxPts];
+            if(i >= k)
+                res += dp[i];
         }
-        // int j = 0;
-        int j = min(0, k-maxPoints-1);
-        for(int i = maxPoints+1; i < k; i++)
-        {
-            percnt -= dp[j];
-            j++;
-            dp[i] = (1.0/maxPoints)*percnt;
-            percnt += dp[i];
-        }
-        double ans = 0;
-        if(n == k  && k == 0)
-            return 1;
-        for(int i = k; i <= n; i++)
-        {
-            if(j >= 0)
-                percnt -= dp[j];
-            j++;
-            dp[i] =  (1.0/maxPoints)*percnt;
-            ans += dp[i];
-        }
-        return ans;
-
+        return res;
     }
 };
