947be517ce326e2cd8cedc6fe89b336c5f356422
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-11 14:42:23 +0530

    Added solution - LeetHub

---
+++ Smallest Subarray GCD - GFG/smallest-subarray-gcd.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int hcf(int a, int b)
+    {
+        if(b > a)
+            return(hcf(b, a));
+        if(b == 0)
+            return a;
+        return hcf(b, a%b);
+            
+    }
+    void buildSeg(vector<int> &seg, int arr[], int idx, int l, int r)
+    {
+        if(l == r)
+        {
+            seg[idx] = arr[l];
+            return;
+        }
+        int mid = (l+r)/2;
+        buildSeg(seg, arr, 2*idx+1, l, mid);
+        buildSeg(seg, arr, 2*idx+2, mid+1, r);
+        seg[idx] = hcf(seg[2*idx+1], seg[2*idx+2]);
+    }
+    int getQuery(vector<int> &seg, int idx, int low, int high, int l, int r)
+    {
+        if(l > high || r < low)
+            return -1;
+        if(high <= r && low >= l)
+            return seg[idx];
+        int mid = (low+high)/2;
+        int op1 = getQuery(seg, 2*idx+1, low, mid, l, r);
+        int op2 = getQuery(seg, 2*idx+2, mid+1, high, l, r);
+        if(op1 == -1)
+            return op2;
+        if(op2 == -1)
+            return op1;
+        return hcf(op1, op2);
+    }
+    int findSmallestSubArr(int arr[], int n, int g) {
+       vector<int> seg(4*n+1);
+    //   cout<<"Tree in progress"<<endl;
+       buildSeg(seg, arr, 0, 0, n-1);
+    //   cout<<"Tree made"<<endl;
+       for(int sz = 1; sz <= n; sz++)
+       {
+           int i = 0;
+           int j = sz-1;
+           while(j < n)
+           {
+              if(getQuery(seg, 0, 0, n-1, i, j) == g)
+              {
+                return sz;
+              }
+                i++;
+                j++;
+            }
+       }
+       return -1;
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, g;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> g;
+        Solution ob;
+        auto ans = ob.findSmallestSubArr(arr, n, g);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
