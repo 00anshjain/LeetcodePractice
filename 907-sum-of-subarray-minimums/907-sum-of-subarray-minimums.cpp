fd18fa2f0ac3e05f9af8e66fbc555d014a9c7f14
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-11 22:41:59 +0530

    Time: 175 ms (28.48%), Space: 42.9 MB (43.58%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
+    typedef long long ll;
+    int mod = 1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int> lmin(n, 0), rmin(n, n-1);
-        stack<int> st1, st2;
-        int mod = 1e9+7;
+        vector<int> NSL(n, -1), NSR(n, n);
+        stack<int> st1;
         for(int i = 0; i < n; i++)
         {
             while(!st1.empty() && arr[st1.top()] > arr[i])
                 st1.pop();
             if(!st1.empty())
-                lmin[i] = st1.top() + 1;
+                NSL[i] = st1.top();
             st1.push(i);
         }
+        stack<int> st2;
         for(int i = n-1; i >= 0; i--)
         {
             while(!st2.empty() && arr[st2.top()] >= arr[i])
                 st2.pop();
             if(!st2.empty())
-                rmin[i] = st2.top() - 1;
+                NSR[i] = st2.top();
             st2.push(i);
         }
-        long long ans = 0;
+        ll sum = 0l;
         for(int i = 0; i < n; i++)
         {
-            ans += (((long long)arr[i] * ((long long)i - lmin[i] + 1)*((long long)rmin[i] - i + 1))%mod );
+            sum += (ll)arr[i]*(i - (ll)NSL[i])*(NSR[i]-i);
+            sum%= mod;
         }
-        return ans%mod;
+        return sum;
         
         
     }
