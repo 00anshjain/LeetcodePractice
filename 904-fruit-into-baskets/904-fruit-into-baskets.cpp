fbafe4017f16ac760e58ce2017ad21ad2c51acf7
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-14 01:28:44 +0530

    Time: 135 ms (95.60%), Space: 66.2 MB (93.33%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int f1 = -1, f2 = -1;
-        //f1 for second last fruit, f2 for last fruit encountered
-        int same_fruit = 0, temp_ans = 0, ans = 0;
-        for(auto f : fruits)
+        int f1 = -1, f2 = -1; //f1 is the main fruit, f2 is second fruit
+        int ans = 0;
+        int samefruit = 0, total = 0;
+        for(auto x : fruits)
         {
-            if(f == f1)
+            if(x == f1)
             {
-                same_fruit = 1; //count for last fruit encountered is 1
-                temp_ans++;
-                f1 = f2;
-                f2 = f; // since last fruit encountered is f
+                samefruit++;
+                total++;
             }
-            else if(f == f2)
+            else if(x == f2)
             {
-                same_fruit += 1;
-                temp_ans++;
+                total++;
+                samefruit = 1;
+                swap(f1, f2);
             }
             else
             {
-                temp_ans = same_fruit + 1; // now answer willl be last fruit same count + 1 for new fruit
-                same_fruit = 1; // new fruit encountered
-                f1 = f2;
-                f2 = f; // new fruit encountered is last fruit
+                total = samefruit+1;
+                f2 = f1;
+                f1 = x;
+                samefruit = 1;
             }
-            ans = max(ans, temp_ans);
+            ans = max(ans, total);
         }
         return ans;
-        
     }
 };
