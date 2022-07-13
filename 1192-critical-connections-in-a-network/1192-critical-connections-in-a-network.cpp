bd4269c5a6df6d6722b1c8c8f9c6e9f326ecf9f3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-13 14:49:40 +0530

    Time: 1235 ms (17.76%), Space: 174.4 MB (81.06%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
     int t = 0;
-    void DFS(int u, vector<int> &dis, vector<int> &low, int par, vector<int> adj[], vector<vector<int>>& res)
+    void DFS(vector<int> &dis, vector<int> &low, int u, vector<int> adj[], vector<vector<int>>& res, int p)
     {
-        dis[u] = t++;
+        dis[u] = ++t;
         low[u] = dis[u];
         for(auto v : adj[u])
         {
-            if(v == par)
+            if(v == p)
                 continue;
             if(dis[v] == -1)
             {
-                DFS(v, dis, low, u, adj, res);
+                DFS(dis, low, v, adj, res, u);
                 low[u] = min(low[u], low[v]);
-                if(dis[u] < low[v]) // mtlb iske paas aur koi rasta nhi h aage jane ka mere alawa
-                    res.push_back({u, v});
             }
-            else
+            if(low[v] > dis[u])
             {
-                low[u] = min(low[u], low[v]);
+                res.push_back({u, v});
+            }
+            else{
+                low[u] = min(low[u], dis[v]);
             }
         }
-        // inStack[u] = false;
-        // st.pop();
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& con) {
+        vector<vector<int>> res;
+        t = 0;
         vector<int> adj[n];
         for(auto x : con)
         {
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
-            
         }
-        vector<int> dis(n, -1); //discoveryTime
-        vector<int> low(n, INT_MAX); //lowTime
-        t = 0;
-        // stack<int> st;
+        vector<int> dis(n, -1), low(n, -1);
         // vector<bool> inStack(n, false);
-        vector<vector<int>> res;
+        // stack<int> st;
         for(int i = 0; i < n; i++)
         {
             if(dis[i] == -1)
-            {
-                DFS(i, dis, low, -1, adj, res);
-            }
+                DFS(dis, low, i, adj, res, -1);
         }
-        
-        
         return res;
-        
     }
 };
