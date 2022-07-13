8bc6c3df06d3c25486d6da105fbb4c0fb65615e9
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-13 18:23:54 +0530

    Time: 55 ms (88.14%), Space: 8.2 MB (60.21%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    int n;
-    int recur(string &s, int i, vector<int> &dp, vector<vector<bool>>& palin)
-    {
-        if(i == n)
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        int ans = INT_MAX;
-        for(int j = i; j < n; j++)
-        {
-            if(palin[i][j])
-            {
-                int k = recur(s, j+1, dp, palin);
-                if(k != INT_MAX)
-                    ans = min(ans, 1+k);
-            }
-        }
-        // if(ans == INT_MAX)
-        //     return dp[i] = INT_MAX;
-        return dp[i] = ans;
-    }
     int minCut(string s) {
-        n = s.size();
-        vector<vector<bool>> palin(n, vector<bool>(n, false));
+        int n = s.size();
+        vector<int> cut(n);
         for(int i = 0; i < n; i++)
+            cut[i] = i;
+        vector<vector<bool>> palin(n, vector<bool>(n, false));
+        for(int j = 0; j < n; j++)//fixing the ending position
         {
-            palin[i][i] = true;
-            if(i+1 < n && (s[i] == s[i+1]))
+            for(int i = j; i >= 0; i--)//changing starting position
             {
-                palin[i][i+1] = true;
+                if(s[i] == s[j] && ((j-i+1) <= 2 || palin[i+1][j-1]))
+                {
+                    palin[i][j] = true;
+                    if(i != 0)
+                        cut[j] = min(cut[j], 1 + cut[i-1]);
+                    else
+                        cut[j] = 0;
+                }
             }
         }
-        for(int i = n-3; i >= 0; i--)
-        {
-            for(int j = i+2; j<n; j++)
-            {
-                if(s[i] == s[j])
-                    palin[i][j] = palin[i+1][j-1];
-            }
-        }
+        return cut[n-1];
         
-        vector<int> dp(n, -1);
-        return recur(s, 0, dp, palin)-1;
+        // vector<int> dp(n, -1);
+        // return recur(s, 0, dp, palin)-1;
     }
 };
