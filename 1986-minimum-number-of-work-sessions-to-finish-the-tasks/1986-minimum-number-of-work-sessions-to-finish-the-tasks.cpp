f7cee968d6fcb6462247f2050582634e86e2ed1b
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-15 23:25:57 +0530

    Time: 664 ms (35.00%), Space: 10.4 MB (55.64%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int ans = INT_MAX;
     int n;
-    int taken[1<<15][15][16];
-    void recur(vector<int>& tasks, int left, int &sessionTime, int s, int currSessTime, int mask)
+    int taken[1<<15][16];
+    int recur(vector<int>& tasks, int &sessionTime, int currSessTime, int mask)
     {
-        if(taken[mask][s][currSessTime])
-            return;
-        if(s >= ans)
-            return;
-        if(left == 0)
+        if(mask == ((1<<n) - 1))
         {
+            // cout<<"here"<<endl;
             //ans = min(ans, s);
-            ans = s;
-            return;
+            return 1;
         }
+        if(taken[mask][currSessTime] != -1)
+            return taken[mask][currSessTime];
+        int ans = INT_MAX;
         for(int i = 0; i < n; i++)
         {
             if(tasks[i] != -1)
@@ -23,22 +22,28 @@ public:
                 tasks[i] = -1;
                 if(currSessTime + k <= sessionTime)
                 {
-                    recur(tasks, left-1, sessionTime, s, currSessTime + k, mask | (1<<i));
+                    ans = min(ans, recur(tasks, sessionTime, currSessTime + k, mask | (1<<i)));
                 }
-                recur(tasks, left-1, sessionTime, s+1, k, mask | (1<<i));
+                else
+                {
+                    
+                    int z = recur(tasks, sessionTime, k, mask | (1<<i));
+                    if(z != INT_MAX)
+                        ans = min(ans, 1+z);
+                }
                 tasks[i] = k;
             }
         }
-        taken[mask][s][currSessTime] = true;
+        return taken[mask][currSessTime] = ans;
         
     }
     int minSessions(vector<int>& tasks, int sessionTime) {
         n = tasks.size();
         int mask = 0;
-        memset(taken, false, sizeof(taken));
+        memset(taken, -1, sizeof(taken));
         // vector<vector<bool>> taken(1<<n, vector<bool>(tasks.size() + 1, false));//maxm session = n
         ans = INT_MAX;
-        recur(tasks, n, sessionTime, 1, 0, mask);
-        return ans;
+        return recur(tasks, sessionTime, 0, mask);
+        // return ans;
     }
 };
