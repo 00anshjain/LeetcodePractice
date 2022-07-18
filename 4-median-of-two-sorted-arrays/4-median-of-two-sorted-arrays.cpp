36f264334cf2426934d2af0ac6356effa293d7fa
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-19 00:59:57 +0530

    Time: 71 ms (34.43%), Space: 89.4 MB (60.88%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -3,30 +3,30 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        int low = 0, high = n;  
         if(n > m)
             return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n;  
         // 1, 3, 4, 5, 8, 10, 12 
        // ^  ^  ^  ^  ^  ^   ^  ^
         // 2, 3, 6, 15
-        int lcount = (n+m+1)/2;//+1 because we want more lements towards left when odd elements total
+        int leftCount = (n+m+1)/2;//+1 because we want more lements towards left when odd elements total
         while(low <= high)
         {
-            int mid = (low+high)/2;
-            int cut2 = lcount - mid; 
+            int cut1 = (low+high)/2;
+            int cut2 = leftCount - cut1; 
             int left1 = INT_MIN, left2 = INT_MIN, right1 = INT_MAX, right2 = INT_MAX;
-            if(mid > 0)
-                left1 = nums1[mid-1];
-            if(mid != n)
-                right1 = nums1[mid];
+            if(cut1 > 0)
+                left1 = nums1[cut1-1];
+            if(cut1 != n)
+                right1 = nums1[cut1];
             if(cut2 != 0)
                 left2 = nums2[cut2 - 1];
             if(cut2 != m)
                 right2 = nums2[cut2];
             if(left1 > right2)
-                high = mid - 1;
+                high = cut1 - 1;
             else if(left2 > right1)
-                low = mid + 1;
+                low = cut1 + 1;
             else
             {
                 if((n+m)%2 == 0)
