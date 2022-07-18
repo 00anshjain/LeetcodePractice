5ead7e37ad7d4e2ae803d46c66fb407257689e7c
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-19 00:53:02 +0530

    Time: 93 ms (10.63%), Space: 89.4 MB (60.88%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double ans = 0;
-        if(nums1.size() > nums2.size())
+        int n = nums1.size();
+        int m = nums2.size();
+        int low = 0, high = n;  
+        if(n > m)
             return findMedianSortedArrays(nums2, nums1);
-        int high = nums1.size();
-        int low = 0;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        while(high >= low)
+        // 1, 3, 4, 5, 8, 10, 12 
+       // ^  ^  ^  ^  ^  ^   ^  ^
+        // 2, 3, 6, 15
+        int lcount = (n+m+1)/2;//+1 because we want more lements towards left when odd elements total
+        while(low <= high)
         {
             int mid = (low+high)/2;
-            int left1 = (mid != 0 ? nums1[mid - 1] : INT_MIN);
-            int right1 = (mid != n1 ? nums1[mid] : INT_MAX);
-            int cut2 = (n1+n2+1)/2 - mid; 
-            // int left2 = (cut2 != 0 ? nums2[cut2 - 1] : INT_MIN);
-            int left2 = (cut2 != 0 ? nums2[cut2 - 1] : INT_MIN);
-            int right2 = (cut2 != n2 ? nums2[cut2] : INT_MAX);
-            
+            int cut2 = lcount - mid; 
+            int left1 = INT_MIN, left2 = INT_MIN, right1 = INT_MAX, right2 = INT_MAX;
+            if(mid > 0)
+                left1 = nums1[mid-1];
+            if(mid != n)
+                right1 = nums1[mid];
+            if(cut2 != 0)
+                left2 = nums2[cut2 - 1];
+            if(cut2 != m)
+                right2 = nums2[cut2];
             if(left1 > right2)
                 high = mid - 1;
             else if(left2 > right1)
                 low = mid + 1;
             else
             {
-                if((n1 + n2) % 2)
-                // {
-                    return (double)max(left1, left2);
-                // }
-                // else
-                // {
-                    return ((double)max(left1, left2) + min(right1, right2))/2;
-                // }
+                if((n+m)%2 == 0)
+                    return ((double)(max(left1, left2)) + min(right1, right2))/2;
+                else
+                    return max(left1, left2);
             }
+            
         }
         return -1;
         
