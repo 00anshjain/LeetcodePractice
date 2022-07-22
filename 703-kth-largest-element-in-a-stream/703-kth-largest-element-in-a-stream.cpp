49e16d0f3dd410c888502d3b4f10e3897d694c89
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-22 20:26:21 +0530

    Time: 1923 ms (5.00%), Space: 32.9 MB (5.01%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,26 +1,71 @@
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int a)
+    {
+        data = a;
+        left = NULL;
+        right = NULL;
+    }
+};
 class KthLargest {
 public:
-    multiset<int, greater<int>> st;
+    Node* head = NULL;
     int K;
+    
+    Node* makeBST(vector<int> &nums, int l, int r)
+    {
+        if(l > r)
+            return NULL;
+        if(l == r)
+            return new Node(nums[l]);
+        int mid = l + (r-l)/2;
+        Node* root = new Node(nums[mid]);
+        root->left = makeBST(nums, l, mid-1);
+        root->right = makeBST(nums, mid+1, r);
+        return root;
+    }
     KthLargest(int k, vector<int>& nums) {
-        // multiset<int, greater<int>> p(nums.begin(), nums.end());
-        for(auto x : nums)
-            st.insert(x);
-        // st = p;
+        sort(nums.begin(), nums.end());
+        head = makeBST(nums, 0, nums.size() - 1);
         K = k;
     }
     
+    Node* addValue(int val, Node* root)
+    {
+        if(!root)
+            return new Node(val);
+        if(root->data < val)
+        {
+            root->right = addValue(val, root->right);
+            return root;
+        }
+        else if(root->data >= val)
+        {
+            root->left = addValue(val, root->left);
+            return root;
+        }
+        return NULL;
+    }
+    int DFS(Node* root, int &k)
+    {
+        if(!root)
+            return -1;
+        int l = DFS(root->right, k);
+        if(l != -1)
+            return l;
+        k--;
+        if(k == 0)
+            return root->data;
+        return DFS(root->left, k);
+    }
     int add(int val) {
-        st.insert(val);
+        head = addValue(val, head);
         int k = K;
         // cout<<k<<endl;
-        for(auto itr = st.begin(); itr != st.end(); itr++)
-        {
-            if(k == 1)
-                return *itr;
-            k--;
-        }
-        return -1;
+        return DFS(head, k);
     }
 };
 
