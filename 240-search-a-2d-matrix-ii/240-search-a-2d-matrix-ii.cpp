3a273da90fa691f4299443a0eddaed198595fbee
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-24 14:11:20 +0530

    Time: 108 ms (91.24%), Space: 14.9 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    bool binarySearch(vector<vector<int>> &matrix, int i, int n, int m, int target)
-    {
-        int left = 0, right = n-1;
-        while(left <= right)
-        {
-            int mid = left + (right - left)/2;
-            if(matrix[mid][i] == target)
-                return true;
-            else if(matrix[mid][i] < target)
-                left = mid +1;
-            else
-                right = mid - 1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        //for selecting colums, we cut down columns by using first and last row
-        int r = upper_bound(matrix[0].begin(), matrix[0].end(), target) - matrix[0].begin();
-        int l = lower_bound(matrix[n-1].begin(), matrix[n-1].end(), target) - matrix[n-1].begin();
-        
-        for(int i = l; i < r; i++)
+        if(target < matrix[0][0] || target > matrix[n-1][m-1])
+            return false;
+        int row = n-1;
+        int col = 0;
+        while(row >= 0 && col < m)
         {
-            if(binarySearch(matrix, i, n, m, target))
+            if(target < matrix[row][col])
+                row--;
+            else if(target > matrix[row][col])
+                col++;
+            else
                 return true;
-            // int k = lower_bound(matrix[0][i].begin(), matrix[].end(), target) - matrix[i].begin();
         }
         return false;
     }
