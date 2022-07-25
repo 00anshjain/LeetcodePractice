94bc8d47aa181e3389800c43140f2d0ba0dc16ec
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-25 15:33:13 +0530

    Time: 39 ms (40.12%), Space: 8 MB (42.21%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,64 +1,46 @@
 class Solution {
 public:
-    bool isCheck(unordered_map<char, int> &tmp, unordered_map<char, int> &smp)
-    {
-        for(auto x : tmp)
-        {
-            if(smp[x.first] < x.second)
-                return false;
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int m = s.size(), n = t.size();
-        unordered_map<char, int> tmp, smp;
-        // string ans = "";
-        int begin = -1, end = -1;
-        int res = INT_MAX;
+        unordered_map<char, int> mp;
+        for(auto c : t)
+            mp[c]++;
+        int n = s.size();
+        int len = INT_MAX;
+        int start = -1;
+        // int start = 0, end = INT_MAX-2;
+        unordered_map<char, int> cmp;
+        int k = mp.size();
+        int unq = 0;
+        int j = 0;
         for(int i = 0; i < n; i++)
         {
-            tmp[t[i]]++;
-            smp[s[i]]++;    
-        }
-        bool flag = false;
-        if(isCheck(tmp, smp))
-        {
-            flag = true;
-            begin = 0;
-            end = n-1;
-            res = n;
-        }
-        int i = 0;
-        for(int j = n; j < m; j++)
-        {
-            smp[s[j]]++;
-            // cout<<s[j]<<"   "<<smp[s[j]]<<endl;
-            if(!flag)
+            int c = s[i];
+            if(mp.find(c) == mp.end())
             {
-                if(isCheck(tmp, smp))
-                    flag = true;
+                cmp[c]++;
+                // continue;
             }
-            if(flag)
+            else
             {
-                while(smp[s[i]] > tmp[s[i]])
+                cmp[c]++;
+                if(cmp[c] == mp[c])
+                    unq++;
+                while(unq == k)
                 {
-                    smp[s[i]]--;
-                    i++;
-                }
-                // cout<<smp[s[i]]<<" "<<tmp[s[i]]<<" "<<i<<" "<<j<<" "<<res<<endl;
-                if(res > j-i+1)
-                {
-                    res = j-i+1;
-                    begin = i;
-                    end = j;
+                    if(len > i-j+1)
+                    {
+                        len = i-j+1;
+                        start = j;
+                    }
+                    if(cmp[s[j]] == mp[s[j]])
+                        unq--;
+                    cmp[s[j]]--;
+                    j++;
                 }
             }
-            // j++;
         }
-        // cout<<res<<endl;
-        if(begin == -1)
+        if(start == -1)
             return "";
-        return s.substr(begin, res);
-        
+        return s.substr(start, len);
     }
 };
