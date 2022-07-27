b9c344bb44043fe88e4825bf750baee4d53273db
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-27 15:16:09 +0530

    Time: 23 ms (14.41%), Space: 10.1 MB (42.70%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     typedef long long ll;
     vector<int> singleNumber(vector<int>& nums) {
-        ll xorr = 0;
+        ll xorr = 0l;
         for(auto x : nums)
             xorr ^= x;
-        // for(auto x : nums)
-        ll RSB = (xorr & -xorr);
+        ll rsb = xorr & -xorr;
         int num1 = 0, num2 = 0;
         for(auto x : nums)
         {
-            if((RSB & x) > 0)
+            if((rsb & x) > 0)
                 num1 ^= x;
             else
                 num2 ^= x;
