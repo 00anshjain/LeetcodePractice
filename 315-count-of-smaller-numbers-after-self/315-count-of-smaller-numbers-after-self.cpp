0b8f25d1135238c1291c361cf33a0f6fff2d86fe
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-07-30 17:37:37 +0530

    Time: 358 ms (87.97%), Space: 85.7 MB (82.39%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,58 +1,60 @@
 class Solution {
 public:
-    void merge(vector<pair<int, int>> &arr, int l, int mid, int r, vector<int>& cnt, vector<pair<int, int>>& temp)
+    void merge(vector<pair<int, int>> &nums, vector<int> &cnt, int l, int m, int r, vector<pair<int, int>>& temp)
     {
+        int i = l, j = m+1, k = l;
         int curr = 0;
         // vector<pair<int, int>> temp(r+1);
-        int i = l, j = mid+1;
-        int k = l;
-        while(i <= mid && j <= r)
+        while(i <= m && j <= r)
         {
-            if(arr[i].first > arr[j].first)
+            if(nums[i].first > nums[j].first)
             {
-                temp[k++] = arr[j++];
-                curr++;//inversion here
+                temp[k++] = nums[j];
+                curr++;
+                // cnt[nums[j].second] += curr;
+                j++;
             }
-            else
+            else// if(nums[i].first < nums[j].first)
             {
-                cnt[arr[i].second] += curr;
-                temp[k++] = arr[i++];
+                temp[k++] = nums[i];
+                cnt[nums[i].second] += curr;
+                i++;
             }
         }
-        while(i <= mid)
+        while(i <= m)
         {
-            cnt[arr[i].second] += curr;
-            temp[k++] = arr[i++];
+            temp[k++] = nums[i];
+            cnt[nums[i].second] += curr;
+            i++;
         }
         while(j <= r)
         {
-            temp[k++] = arr[j++];
+            temp[k++] = nums[j];
+            // cnt++;
+            // cnt[nums[j].second] += curr;
+            j++;
         }
-        for(int i =l; i <= r; i++)
-        {
-            arr[i] = temp[i];
-        }
-    }
-    void mergeSort(vector<pair<int, int>> &arr, int l, int r, vector<int>& cnt, vector<pair<int, int>>& temp)
-    {
-        if(l >= r)
-            return;
-        int mid = (l+r)/2;
-        mergeSort(arr, l, mid, cnt, temp);
-        mergeSort(arr, mid+1, r, cnt, temp);
-        merge(arr, l, mid, r, cnt, temp);
+        for(int i = l; i <= r; i++)
+            nums[i] = temp[i];
         
+    }
+    void mergesort(vector<pair<int, int>> &nums, vector<int> &cnt, int l, int r, vector<pair<int, int>>& temp)
+    {
+        if(r <= l)
+            return;
+        int m = (l+r)/2;
+        mergesort(nums, cnt, l, m, temp);
+        mergesort(nums, cnt, m+1, r, temp);
+        merge(nums, cnt, l, m, r, temp);
     }
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<int> cnt(n, 0);
-        vector<pair<int, int>> arr;
+        vector<pair<int, int>> num;
         vector<pair<int, int>> temp(n+1);
-        for(int i = 0; i < n; i++)
-        {
-            arr.push_back({nums[i], i});
-        }
-        mergeSort(arr, 0, n-1, cnt, temp);
+        for(int i = 0; i <  n; i++)
+            num.push_back({nums[i], i});
+        vector<int> cnt(n, 0);
+        mergesort(num, cnt, 0, n-1, temp);
         return cnt;
     }
 };
