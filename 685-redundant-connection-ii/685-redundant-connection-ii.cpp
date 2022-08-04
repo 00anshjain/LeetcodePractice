5b06111a686fd39365737f58b63a9a521d83ccb3
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-08-04 11:10:49 +0530

    Time: 11 ms (77.48%), Space: 9.6 MB (53.03%) - LeetHub

--- 685-redundant-connection-ii/685-redundant-connection-ii.cpp
+++ 685-redundant-connection-ii/685-redundant-connection-ii.cpp
@@ -1,72 +1,51 @@
 class Solution {
 public:
-    vector<int> par, sz;
-    int findPar(int a)
+    int findPar(vector<int> &par, int p)
     {
-        if(par[a] == a)
-            return a;
-        return par[a] = findPar(par[a]);
+        if(par[p] == p)
+            return p;
+        return par[p] = findPar(par, par[p]);
     }
-    void unionSet(int p1, int p2)
+    bool unionPar(vector<int>& par, int a, int b)
     {
-        // par[p2] = p1;
-        if(sz[p2] > sz[p2])
-        {
-            par[p1] = p2;
-            sz[p2] += sz[p1]; 
-        }
-        else
-        {
-            par[p2] = p1;
-            sz[p1] += p2;
-        }
+        int p1 = findPar(par, a);
+        int p2 = findPar(par, b);
+        if(p1 == p2)
+            return false;
+        par[p1] = p2;
+        return true;
     }
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        par.resize(n+1);
-        sz.resize(n+1);
-        for(int i = 1; i <= n; i++)
-        {
-            par[i] = i;
-            sz[i] = 1;
-        }
         vector<int> arr(n+1, -1);
-        int black1 = -1, black2 = -1;//for two parents of a node, we blacklistr both the edges
+        int e1 = -1, e2 = -1;
         for(int i = 0; i < n; i++)
         {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            if(arr[v] != -1)
+            auto x = edges[i];
+            if(arr[x[1]] != -1)
             {
-                black1 = i; //we store index of edge that is creating problem
-                black2 = arr[v];
+                e1 = arr[x[1]];
+                e2 = i;
                 break;
             }
-            arr[v] = i;
+            else
+                arr[x[1]] = i;
         }
-        vector<int> ans;
+        vector<int> par(n+1, -1);
+        for(int i = 1; i <= n; i++)
+            par[i]  = i;
         for(int i = 0; i < n; i++)
         {
-            vector<int> x = edges[i];
-            if(i == black1)
-                continue; 
-            //that we check if cycle exist after removing black1 also, then black2 is responsible for it
-            //so we remove black2 then
-            //else remove black1
-            //ok if black1 == -1, means no node with two parents, then remove edge which lead to form cycle
-            int p1 = findPar(x[0]);
-            int p2 = findPar(x[1]);
-            if(p1 == p2)
+            if(i == e2)
+                continue;
+            if(!unionPar(par, edges[i][0], edges[i][1]))
             {
-                //cycle occured even after removing black1
-                //if black1 == -1, no edge leading to two parent of a node
-                if(black2 == -1)
-                    return x;
-                return edges[black2];
+                if(e1 != -1)
+                    return edges[e1];
+                return edges[i];
             }
-            unionSet(p1, p2);
         }
-        return edges[black1];
-        // return ans;
+        return edges[e2];
+        
     }
 };
