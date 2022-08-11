4c8d7be690a1b8e33d6198b7741ba369e5d54e0d
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-08-11 17:19:09 +0530

    Time: 3 ms (89.06%), Space: 8.1 MB (33.68%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    string shortestPalindrome(string s) {
-        string g(s);
-        int n = s.size();
-        reverse(g.begin(), g.end());
-        string v = s + '#' + g;
+    string shortestPalindrome(string v) {
+        string g(v);
         int sz = v.size();
-        vector<int> pi(sz, 0);
-        // int j = 0;
-        for(int i = 1; i < sz; i++)
+        reverse(g.begin(), g.end());
+        string s = v + "#" + g;
+        int n = s.size();
+        // for(int i = 0; i < n; i++)
+        vector<int> pi(n, 0);
+        for(int i = 1; i < n; i++)
         {
             int j = pi[i-1];
-            while(j > 0 && v[i] != v[j])
+            while(j > 0 && s[j] != s[i])
                 j = pi[j-1];
-            if(v[i] == v[j])
+            if(s[i] == s[j])
                 j++;
-            pi[i] = j;  
+            pi[i] = j;
         }
-        return g.substr(0, n-pi[sz-1]) + s;
+        return g.substr(0, sz - pi[n-1]) + v;
+        
     }
 };
