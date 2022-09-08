0788902e4f17b33c6dbed8b50482738abb2bf9cd
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-09-08 12:00:30 +0530

    Time: 222 ms (21.38%), Space: 36.1 MB (43.90%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<vector<int>> r(n, vector<int>(n, 0));
+        // vector<vector<int>> r(n, vector<int>(n, 0));
+        //removed this because Each pair of cities has at most one road connecting them.
         vector<int> total(n, 0); //total number of roads from one vertex
+        unordered_set<int> st; 
+        //we will make a number from vertices and add it to set
+        //doing this so that we can use unorderedSet
         for(auto x : roads)
         {
-            r[x[0]][x[1]]++;
-            r[x[1]][x[0]]++;
+            // r[x[0]][x[1]]++;
+            // r[x[1]][x[0]]++;
             total[x[0]]++;
             total[x[1]]++;
+            st.insert((min(x[0], x[1])<<16) + max(x[0], x[1]));
         }
         int ans = 0;
         for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = i+1; j < n; j++)
             {
-                if(i == j)
-                    continue;
-               ans = max(ans, total[i] + total[j] - r[i][j]); 
+                // if(i == j)
+                //     continue;
+               ans = max(ans, total[i] + total[j] - (int)st.count((i<<16) + j)); 
             }
         }
         // for(auto x : roads)
