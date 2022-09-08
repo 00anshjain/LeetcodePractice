617f45406107a2deab224062737616c132e038cd
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-09-08 17:51:55 +0530

    Time: 3 ms (43.80%), Space: 6.3 MB (8.09%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -16,7 +16,7 @@ public:
             else
                 dir = right[dir];
         }
-        return (x == 0 && y == 0);
+        return (x == 0 && y == 0) || dir != 'N';
     }
     bool isRobotBounded(string s) {
         unordered_map<char, char> left, right;
@@ -24,12 +24,13 @@ public:
         go['N'] = {0, 1}; go['E'] = {-1, 0}; go['S'] = {0, -1}; go['W'] = {1, 0};
         left['N'] = 'W'; left['W'] = 'S'; left['S'] = 'E'; left['E'] = 'N';
         right['N'] = 'E'; right['E'] = 'S'; right['S'] = 'W'; right['W'] = 'N';
-        string s2 = s + s;
-        string s3 = s2 + s;
-        string s4 = s3 + s;
+        // string s2 = s + s;
+        // string s3 = s2 + s;
+        // string s4 = s3 + s;
         // for(int i = 0; i < 4; i++)
         // {
-        if(check(s, left, right, go) || check(s2, left, right, go) || check(s3, left, right, go) || check(s4, left, right, go))
+        // if(check(s, left, right, go) || check(s2, left, right, go) || check(s3, left, right, go) || check(s4, left, right, go))
+        if(check(s, left, right, go))
             return true;
         return false;    
             
