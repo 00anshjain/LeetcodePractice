5ec21a523259dbcc58c7b06e032d36502a5d6a48
00anshjain <54469378+00anshjain@users.noreply.github.com>
2022-09-27 17:13:10 +0530

    Time: 343 ms (9.79%), Space: 20.7 MB (46.10%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int uniqueLetterString(string s) {
         int n = s.size();
-        // vector<vector<int>> pre(n+1,vector<int>(26,0));
-        vector<int> left(n,-1), right(n, n);
+        vector<int> left(n, -1), right(n, n);
         unordered_map<char, int> mp;
         for(int i = 0; i < n; i++)
         {
-            if(mp.find(s[i]) != mp.end())
+            if(mp.count(s[i]))
             {
                 left[i] = mp[s[i]];
             }
@@ -16,16 +15,16 @@ public:
         mp.clear();
         for(int i = n-1; i >= 0; i--)
         {
-            if(mp.find(s[i]) != mp.end())
+            if(mp.count(s[i]))
             {
                 right[i] = mp[s[i]];
             }
             mp[s[i]] = i;
         }
         int ans = 0;
-        for(int i= 0; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
-            ans += (i - left[i])*(right[i] - i);
+            ans += (i - left[i]) * (right[i] - i);
         }
         return ans;
     }
